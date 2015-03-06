4ecf22b67cbf362d53362da5983e587dde7361af
Spencer Gibb <spencer@gibb.us>
2015-03-06 14:26:20 -0700

    formatting

--- spring-cloud-sleuth-sample/src/main/java/org/springframework/cloud/sleuth/sample/SampleApplication.java
+++ spring-cloud-sleuth-sample/src/main/java/org/springframework/cloud/sleuth/sample/SampleApplication.java
@@ -1,6 +1,7 @@
 package org.springframework.cloud.sleuth.sample;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -18,17 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SampleApplication {
 
-    public static final String CLIENT_NAME = "testApp";
+	public static final String CLIENT_NAME = "testApp";
 
-    @Autowired
-    Environment env;
+	@Autowired
+	Environment env;
 
-    @RequestMapping("/")
-    public String hi() {
-        return "hi";
-    }
+	@RequestMapping("/")
+	public String hi() {
+		return "hi";
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
 }
