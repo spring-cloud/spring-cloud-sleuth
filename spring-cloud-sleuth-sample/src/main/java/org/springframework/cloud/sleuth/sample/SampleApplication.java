fcd1ada1ecc387b07b80bc256ec54f50fea7a302
Spencer Gibb <spencer@gibb.us>
2015-08-03 16:02:09 -0600

    Allow sample to enable log collector with a property.
    Property is sleuth.sample.logging.collector.enabled=true

--- spring-cloud-sleuth-sample/src/main/java/org/springframework/cloud/sleuth/sample/SampleApplication.java
+++ spring-cloud-sleuth-sample/src/main/java/org/springframework/cloud/sleuth/sample/SampleApplication.java
@@ -1,7 +1,10 @@
 package org.springframework.cloud.sleuth.sample;
 
+import com.github.kristofa.brave.LoggingSpanCollectorImpl;
+import com.github.kristofa.brave.SpanCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
@@ -33,9 +36,10 @@ public class SampleApplication {
 	}
 
 	// Use this for debugging (or if there is no Zipkin collector running on port 9410)
-	//	@Bean
-	//	public SpanCollector spanCollector() {
-	//		return new LoggingSpanCollectorImpl();
-	//	}
+	@Bean
+	@ConditionalOnProperty("sleuth.sample.logging.collector.enabled")
+	public SpanCollector spanCollector() {
+		return new LoggingSpanCollectorImpl();
+	}
 
 }
