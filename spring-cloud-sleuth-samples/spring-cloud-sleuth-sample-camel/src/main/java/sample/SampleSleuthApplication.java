b63c255391b54893b6b253c70060c255876b7ec6
Alex Collins <alex.e.c@gmail.com>
2015-11-14 14:16:00 +0000

    copied basic sample to convert to camel

---
+++ spring-cloud-sleuth-samples/spring-cloud-sleuth-sample-camel/src/main/java/sample/SampleSleuthApplication.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Spencer Gibb
+ */
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAsync
+public class SampleSleuthApplication {
+
+	public static final String CLIENT_NAME = "testApp";
+
+	@Bean
+	public Sampler<?> defaultSampler() {
+		return new AlwaysSampler();
+	}
+
+	@Bean
+	public SampleController sampleController() {
+		return new SampleController();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSleuthApplication.class, args);
+	}
+
+}
