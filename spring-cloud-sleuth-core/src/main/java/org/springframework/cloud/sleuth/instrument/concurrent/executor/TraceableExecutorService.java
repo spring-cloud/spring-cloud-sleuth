c588d73552e270e789b02a11178345c20b476079
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2015-11-24 22:23:30 +0530

    Providing support to enable tracing in Executors
    User can opt to wrap java.unit.concurrent package usage with a
    traceable version of the same thing.
    
    Fixes gh-60, fixes gh-58

---
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/concurrent/executor/TraceableExecutorService.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.sleuth.instrument.concurrent.executor;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.cloud.sleuth.Trace;
+import org.springframework.cloud.sleuth.instrument.TraceCallable;
+import org.springframework.cloud.sleuth.instrument.TraceRunnable;
+/**
+ * A decorator class for {@link ExecutorService} to support tracing in Executors
+ * @author Gaurav Rai Mazra
+ *
+ */
+public class TraceableExecutorService implements ExecutorService {
+	final ExecutorService delegate;
+	final Trace trace;
+	
+	public TraceableExecutorService(final ExecutorService delegate, final Trace trace) {
+		this.delegate = delegate;
+		this.trace = trace;
+	}
+	
+	@Override
+	public void execute(Runnable command) {
+		final Runnable r = new TraceRunnable(trace, command);
+		this.delegate.execute(r);
+	}
+
+	@Override
+	public void shutdown() {
+		this.delegate.shutdown();
+	}
+
+	@Override
+	public List<Runnable> shutdownNow() {
+		return this.delegate.shutdownNow();
+	}
+
+	@Override
+	public boolean isShutdown() {
+		return this.delegate.isShutdown();
+	}
+
+	@Override
+	public boolean isTerminated() {
+		return this.delegate.isTerminated();
+	}
+
+	@Override
+	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+		return this.delegate.awaitTermination(timeout, unit);
+	}
+
+	@Override
+	public <T> Future<T> submit(Callable<T> task) {
+		Callable<T> c = new TraceCallable<>(this.trace, task);
+		return this.delegate.submit(c);
+	}
+
+	@Override
+	public <T> Future<T> submit(Runnable task, T result) {
+		Runnable r = new TraceRunnable(trace, task);
+		return this.delegate.submit(r, result);
+	}
+
+	@Override
+	public Future<?> submit(Runnable task) {
+		Runnable r = new TraceRunnable(trace, task);
+		return this.delegate.submit(r);
+	}
+
+	@Override
+	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+		return this.delegate.invokeAll(tasks);
+	}
+
+	@Override
+	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+			throws InterruptedException {
+		return this.delegate.invokeAll(tasks, timeout, unit);
+	}
+
+	@Override
+	public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+		return this.delegate.invokeAny(tasks);
+	}
+
+	@Override
+	public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+			throws InterruptedException, ExecutionException, TimeoutException {
+		return this.delegate.invokeAny(tasks, timeout, unit);
+	}
+
+}
