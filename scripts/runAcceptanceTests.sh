c49ee09b685fee1e530fe9366c7189012b31ae84
Dave Syer <dsyer@pivotal.io>
2015-12-16 10:57:11 +0000

    Zipkin java bumped to 0.1.1

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -2,12 +2,20 @@
 
 set -o errexit
 
+mkdir -p target
+
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
 AT_WHAT_TO_TEST="SLEUTH"
 AT_VERSION="1.0.0.BUILD-SNAPSHOT"
 
-curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
+curl "${SCRIPT_URL}" --output target/runDockerAcceptanceTests.sh
+
+cd target
 
 chmod +x runDockerAcceptanceTests.sh
+
+./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
+
+AT_WHAT_TO_TEST="SLEUTH_ZIPKIN"
 
 ./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
