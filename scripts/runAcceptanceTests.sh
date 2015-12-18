b48f00f67707abb010568977f26ef2fceb4ca469
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-12-18 23:14:47 +0100

    Removed version param to use default BOM Brixton Snapshot version

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -6,7 +6,6 @@ mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
 AT_WHAT_TO_TEST="SLEUTH"
-AT_VERSION="1.0.0.BUILD-SNAPSHOT"
 
 curl "${SCRIPT_URL}" --output target/runDockerAcceptanceTests.sh
 
@@ -14,9 +13,9 @@ cd target
 
 chmod +x runDockerAcceptanceTests.sh
 
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
+./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}"
 (cd brewery; docker-compose -f docker-compose-"${AT_WHAT_TO_TEST}".yml kill)
 
 AT_WHAT_TO_TEST="SLEUTH_STREAM"
 
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
+./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}"
