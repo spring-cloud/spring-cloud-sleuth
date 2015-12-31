aa00265c8b435d508fee42117831accb07605bae
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-12-31 14:35:24 +0100

    Fixed the separate script for Stream e2e

--- scripts/runAcceptanceTestsStream.sh
+++ scripts/runAcceptanceTestsStream.sh
@@ -5,18 +5,12 @@ set -o errexit
 mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
-AT_WHAT_TO_TEST="SLEUTH"
+AT_WHAT_TO_TEST="SLEUTH_STREAM"
 
 curl "${SCRIPT_URL}" --output target/runDockerAcceptanceTests.sh
 
 cd target
 
 chmod +x runDockerAcceptanceTests.sh
-
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}"
-
-(cd brewery; docker-compose -f docker-compose-"${AT_WHAT_TO_TEST}".yml kill)
-
-AT_WHAT_TO_TEST="SLEUTH_STREAM"
 
 ./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}"
