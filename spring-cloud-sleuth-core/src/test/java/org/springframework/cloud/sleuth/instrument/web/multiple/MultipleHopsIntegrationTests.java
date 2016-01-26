2ef5ba2a80e7ba09e5ea37c28b3ed4f52120dbbc
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-01-26 13:50:45 +0100

    Fixed checkstyle issues

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/web/multiple/MultipleHopsIntegrationTests.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/web/multiple/MultipleHopsIntegrationTests.java
@@ -1,8 +1,5 @@
 package org.springframework.cloud.sleuth.instrument.web.multiple;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
