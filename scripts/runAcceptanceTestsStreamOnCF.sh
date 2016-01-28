35f1b0c4961e195d368dde21b7ff760e6cc6a967
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-01-28 13:36:53 +0100

    Removed the build directory var export

--- scripts/runAcceptanceTestsStreamOnCF.sh
+++ scripts/runAcceptanceTestsStreamOnCF.sh
@@ -6,7 +6,6 @@ mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/runAcceptanceTests.sh"
 AT_WHAT_TO_TEST="SLEUTH_STREAM"
-export BUILD_DIRECTORY="brewery"
 
 cd target
 
