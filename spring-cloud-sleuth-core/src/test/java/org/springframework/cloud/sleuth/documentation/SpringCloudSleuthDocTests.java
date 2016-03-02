bb97c7bf9d208923af612575d18da915a20d5afc
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-03-02 19:18:18 +0100

    Updated readme with IDE profile, added more docs

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/documentation/SpringCloudSleuthDocTests.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/documentation/SpringCloudSleuthDocTests.java
@@ -16,6 +16,7 @@
 
 package org.springframework.cloud.sleuth.documentation;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,8 +33,13 @@ import org.springframework.cloud.sleuth.SpanNamer;
 import org.springframework.cloud.sleuth.TraceRunnable;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.cloud.sleuth.trace.DefaultTracer;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.cloud.sleuth.assertions.SleuthAssertions.then;
 
 /**
  * Test class to be embedded in the
@@ -104,5 +110,111 @@ public class SpringCloudSleuthDocTests {
 		// end::span_name_to_string_runnable_execution[]
 
 		BDDMockito.then(tracer).should().joinTrace(BDDMockito.eq("calculateTax"), BDDMockito.any(Span.class));
+		executorService.shutdown();
+	}
+
+	ApplicationEventPublisher publisher = Mockito.mock(ApplicationEventPublisher.class);
+	Tracer tracer = new DefaultTracer(new AlwaysSampler(), new Random(), this.publisher, new DefaultSpanNamer());
+
+	@Test
+	public void should_create_a_span_with_tracer() {
+		String taxValue = "10";
+
+		// tag::manual_span_creation[]
+		// Start a span. If there was a span present in this thread it will become
+		// the `newSpan`'s parent.
+		Span newSpan = this.tracer.startTrace("calculateTax");
+		try {
+			// ...
+			// You can tag a span
+			this.tracer.addTag("taxValue", taxValue);
+			// ...
+			// You can log an event on a span
+			newSpan.logEvent("taxCalculated");
+		} finally {
+			// Once done remember to close the span. This will allow collecting
+			// the span to send it to Zipkin
+			this.tracer.close(newSpan);
+		}
+		// end::manual_span_creation[]
+
+		then(this.tracer.getCurrentSpan()).isNull();
+		then(newSpan).isNotNull();
+		then(newSpan.getAccumulatedMillis()).isGreaterThan(0);
+	}
+
+	@Test
+	public void should_continue_a_span_with_tracer() throws Exception {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		String taxValue = "10";
+		Span initialSpan = this.tracer.startTrace("calculateTax");
+		assertThat(initialSpan.tags()).doesNotContainKeys("taxValue");
+		assertThat(initialSpan.logs()).extracting("event").doesNotContain("taxCalculated");
+
+		executorService.submit(() -> {
+			// tag::manual_span_continuation[]
+			// let's assume that we're in a thread Y and we've received
+			// the `initialSpan` from thread X
+			Span continuedSpan = this.tracer.continueSpan(initialSpan);
+			try {
+				// ...
+				// You can tag a span
+				this.tracer.addTag("taxValue", taxValue);
+				// ...
+				// You can log an event on a span
+				continuedSpan.logEvent("taxCalculated");
+			} finally {
+				// Once done remember to detach the span. That way you'll
+				// safely remove it from the current thread without closing it
+				this.tracer.detach(continuedSpan);
+			}
+			// end::manual_span_continuation[]
+		}
+		).get();
+
+		this.tracer.close(initialSpan);
+		then(this.tracer.getCurrentSpan()).isNull();
+		then(initialSpan)
+				.hasATag("taxValue", taxValue)
+				.hasLoggedAnEvent("taxCalculated");
+		executorService.shutdown();
+	}
+
+	@Test
+	public void should_join_a_span_with_tracer() throws Exception {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		String commissionValue = "10";
+		Span initialSpan = this.tracer.startTrace("calculateTax");
+		assertThat(initialSpan.tags()).doesNotContainKeys("commissionValue");
+		assertThat(initialSpan.logs()).extracting("event").doesNotContain("commissionCalculated");
+
+		executorService.submit(() -> {
+			// tag::manual_span_joining[]
+			// let's assume that we're in a thread Y and we've received
+			// the `initialSpan` from thread X. `initialSpan` will be the parent
+			// of the `joinedSpan`
+			Span joinedSpan = this.tracer.joinTrace("calculateCommission", initialSpan);
+			try {
+				// ...
+				// You can tag a span
+				this.tracer.addTag("commissionValue", commissionValue);
+				// ...
+				// You can log an event on a span
+				joinedSpan.logEvent("commissionCalculated");
+			} finally {
+				// Once done remember to close the span. This will allow collecting
+				// the span to send it to Zipkin. The tags and events set on the
+				// joinedSpan will not be present on the parent
+				this.tracer.close(joinedSpan);
+			}
+			// end::manual_span_joining[]
+		}
+		).get();
+
+		this.tracer.close(initialSpan);
+		then(this.tracer.getCurrentSpan()).isNull();
+		assertThat(initialSpan.tags()).doesNotContainKeys("commissionValue");
+		assertThat(initialSpan.logs()).extracting("event").doesNotContain("commissionCalculated");
+		executorService.shutdown();
 	}
 }
