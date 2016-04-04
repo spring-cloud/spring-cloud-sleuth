beafc3a5a752b80d407dd0c121489493a32ac5d1
Shivang Shah <shivangshah@users.noreply.github.com>
2016-04-04 07:40:45 -0700

    Support rx java
    RxJava Support
    
    fixes #235

---
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/rxjava/RxJavaAutoConfiguration.java
@@ -0,0 +1,32 @@
+package org.springframework.cloud.sleuth.instrument.rxjava;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.sleuth.TraceKeys;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rx.plugins.RxJavaSchedulersHook;
+
+/**
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-configuration} that
+ * enables support for RxJava via {@link RxJavaSchedulersHook}.
+ *
+ * @author Shivang Shah
+ * @since 1.0.0
+ */
+@Configuration
+@AutoConfigureAfter(TraceAutoConfiguration.class)
+@ConditionalOnBean(Tracer.class)
+@ConditionalOnClass(RxJavaSchedulersHook.class)
+@ConditionalOnProperty(value = "spring.sleuth.rxjava.schedulers.hook.enabled", matchIfMissing = true)
+public class RxJavaAutoConfiguration {
+
+	@Bean
+	SleuthRxJavaSchedulersHook sleuthRxJavaSchedulersHook(Tracer tracer, TraceKeys traceKeys) {
+		return new SleuthRxJavaSchedulersHook(tracer, traceKeys);
+	}
+}
---
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/rxjava/SleuthRxJavaSchedulersHook.java
@@ -0,0 +1,112 @@
+package org.springframework.cloud.sleuth.instrument.rxjava;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.TraceKeys;
+import org.springframework.cloud.sleuth.Tracer;
+import rx.functions.Action0;
+import rx.plugins.RxJavaErrorHandler;
+import rx.plugins.RxJavaObservableExecutionHook;
+import rx.plugins.RxJavaSchedulersHook;
+import rx.plugins.SleuthRxJavaPlugins;
+
+/**
+ * {@link RxJavaSchedulersHook} that wraps a {@link Action0} into its tracing
+ * representation.
+ *
+ * @author Shivang Shah
+ * @since 1.0.0
+ */
+class SleuthRxJavaSchedulersHook extends RxJavaSchedulersHook {
+
+	private static final Log log = LogFactory.getLog(SleuthRxJavaSchedulersHook.class);
+
+	private static final String RXJAVA_COMPONENT = "rxjava";
+	private final Tracer tracer;
+	private final TraceKeys traceKeys;
+	private RxJavaSchedulersHook delegate;
+
+	SleuthRxJavaSchedulersHook(Tracer tracer, TraceKeys traceKeys) {
+		this.tracer = tracer;
+		this.traceKeys = traceKeys;
+		try {
+			this.delegate = SleuthRxJavaPlugins.getInstance().getSchedulersHook();
+			if (this.delegate instanceof SleuthRxJavaSchedulersHook) {
+				return;
+			}
+			RxJavaErrorHandler errorHandler = SleuthRxJavaPlugins.getInstance().getErrorHandler();
+			RxJavaObservableExecutionHook observableExecutionHook
+				= SleuthRxJavaPlugins.getInstance().getObservableExecutionHook();
+			logCurrentStateOfRxJavaPlugins(errorHandler, observableExecutionHook);
+			SleuthRxJavaPlugins.resetPlugins();
+			SleuthRxJavaPlugins.getInstance().registerSchedulersHook(this);
+			SleuthRxJavaPlugins.getInstance().registerErrorHandler(errorHandler);
+			SleuthRxJavaPlugins.getInstance().registerObservableExecutionHook(observableExecutionHook);
+		} catch (Exception e) {
+			log.error("Failed to register Sleuth RxJava SchedulersHook", e);
+		}
+	}
+
+	private void logCurrentStateOfRxJavaPlugins(RxJavaErrorHandler errorHandler,
+		RxJavaObservableExecutionHook observableExecutionHook) {
+		log.debug("Current RxJava plugins configuration is ["
+			+ "schedulersHook [" + this.delegate + "],"
+			+ "errorHandler [" + errorHandler + "],"
+			+ "observableExecutionHook [" + observableExecutionHook + "],"
+			+ "]");
+		log.debug("Registering Sleuth RxJava Schedulers Hook.");
+	}
+
+	@Override
+	public Action0 onSchedule(Action0 action) {
+		if (action instanceof TraceAction) {
+			return action;
+		}
+		Action0 wrappedAction = this.delegate != null
+			? this.delegate.onSchedule(action) : action;
+		if (wrappedAction instanceof TraceAction) {
+			return action;
+		}
+		return super.onSchedule(new TraceAction(this.tracer, this.traceKeys, wrappedAction));
+	}
+
+	static class TraceAction implements Action0 {
+
+		private final Action0 actual;
+		private Tracer tracer;
+		private TraceKeys traceKeys;
+		private Span parent;
+
+		public TraceAction(Tracer tracer, TraceKeys traceKeys, Action0 actual) {
+			this.tracer = tracer;
+			this.traceKeys = traceKeys;
+			this.parent = tracer.getCurrentSpan();
+			this.actual = actual;
+		}
+
+		@Override
+		public void call() {
+			Span span = this.parent;
+			boolean created = false;
+			if (span != null) {
+				span = this.tracer.continueSpan(span);
+			} else {
+				span = this.tracer.createSpan(RXJAVA_COMPONENT);
+				this.tracer.addTag(Span.SPAN_LOCAL_COMPONENT_TAG_NAME, RXJAVA_COMPONENT);
+				this.tracer.addTag(this.traceKeys.getAsync().getPrefix()
+					+ this.traceKeys.getAsync().getThreadNameKey(), Thread.currentThread().getName());
+				created = true;
+			}
+			try {
+				this.actual.call();
+			} finally {
+				if (created) {
+					this.tracer.close(span);
+				} else {
+					this.tracer.detach(span);
+				}
+			}
+		}
+	}
+}
