47f74c7500b31ecfabe1713d7fdb97bc2d148e73
Dave Syer <dsyer@pivotal.io>
2016-05-06 15:21:31 +0100

    Ignore adhoc tests again

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/AdhocTestSuite.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/AdhocTestSuite.java
@@ -16,6 +16,7 @@
 
 package org.springframework.cloud.sleuth;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -30,7 +31,7 @@ import org.springframework.cloud.sleuth.instrument.web.client.WebClientDiscovery
 @RunWith(Suite.class)
 @SuiteClasses({ 		WebClientDiscoveryExceptionTests.class,
 		RestTemplateTraceAspectIntegrationTests.class })
-// @Ignore
+@Ignore
 public class AdhocTestSuite {
 
 }
