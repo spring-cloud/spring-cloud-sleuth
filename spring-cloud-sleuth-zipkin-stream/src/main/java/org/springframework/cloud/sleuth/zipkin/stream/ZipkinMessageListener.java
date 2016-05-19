650bf6d1f8aa3762cd2881cb0bf5dce0023cd442
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-19 17:23:39 +0200

    Removing the cloud profiled beans for ZipkinMessageListener
    fixes #268

--- spring-cloud-sleuth-zipkin-stream/src/main/java/org/springframework/cloud/sleuth/zipkin/stream/ZipkinMessageListener.java
+++ spring-cloud-sleuth-zipkin-stream/src/main/java/org/springframework/cloud/sleuth/zipkin/stream/ZipkinMessageListener.java
@@ -1,39 +1,22 @@
 package org.springframework.cloud.sleuth.zipkin.stream;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.sleuth.Log;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.stream.SleuthSink;
 import org.springframework.cloud.sleuth.stream.Spans;
 import org.springframework.cloud.sleuth.zipkin.stream.ZipkinMessageListener.NotSleuthStreamClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
-
 import zipkin.Annotation;
 import zipkin.AsyncSpanConsumer;
 import zipkin.BinaryAnnotation;
@@ -43,6 +26,11 @@ import zipkin.CollectorSampler;
 import zipkin.Endpoint;
 import zipkin.Span.Builder;
 import zipkin.StorageComponent;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A message listener that is turned on if Sleuth Stream is disabled.
@@ -134,37 +122,4 @@ public class ZipkinMessageListener {
 		}
 
 	}
-
-	@Configuration
-	@Profile("cloud")
-	protected static class CloudDataSourceConfiguration {
-
-		@Bean
-		public Cloud cloud() {
-			return new CloudFactory().getCloud();
-		}
-
-		@Value("${zipkin.collector.sample-rate:1.0}")
-		float sampleRate = 1.0f;
-
-		@Bean
-		@ConditionalOnMissingBean
-		CollectorSampler collectorSampler() {
-			return CollectorSampler.create(this.sampleRate);
-		}
-
-		@Bean
-		@ConditionalOnMissingBean
-		CollectorMetrics collectorMetrics() {
-			return CollectorMetrics.NOOP_METRICS;
-		}
-
-		@Bean
-		@ConfigurationProperties(DataSourceProperties.PREFIX)
-		public DataSource dataSource() {
-			return cloud().getSingletonServiceConnector(DataSource.class, null);
-		}
-
-	}
-
 }
