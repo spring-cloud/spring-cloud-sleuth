76b8716dc6f737926f80a01601758cb48deb8655
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-24 12:36:25 +0200

    Added Kafka script

---
+++ scripts/runAcceptanceTestsStreamKafka.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+
+mkdir -p target
+
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/runAcceptanceTests.sh"
+AT_WHAT_TO_TEST="SLEUTH_STREAM"
+
+cd target
+
+curl "${SCRIPT_URL}" --output runAcceptanceTests.sh
+
+chmod +x runAcceptanceTests.sh
+
+echo "Killing all running apps"
+./runAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -n
+
+./runAcceptanceTests.sh --kafka --whattotest "${AT_WHAT_TO_TEST}" --killattheend
