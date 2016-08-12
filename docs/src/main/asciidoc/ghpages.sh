e52fc2c75817839a994d21e8d7518580fb9c7913
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-12 13:40:10 +0200

    WIP

--- docs/src/main/asciidoc/ghpages.sh
+++ docs/src/main/asciidoc/ghpages.sh
@@ -70,7 +70,7 @@ if [[ "${CURRENT_BRANCH}" == "master" ]] ; then
             cp -rf $f ${ROOT_FOLDER}/${VERSION_VALUE}
             # We want users to access 1.0.0.RELEASE/ instead of 1.0.0.RELEASE/spring-cloud.sleuth.html
             if [[ "${file}" == "${MAIN_ADOC_VALUE}.html" ]] ; then
-                ln -s ${ROOT_FOLDER}/${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
+                ln -s ${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
                 git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
             fi
             git add -A $file
@@ -85,7 +85,7 @@ else
             # Not ignored...
             cp -rf $f ${ROOT_FOLDER}/${VERSION_VALUE}
             if [[ "${file}" == "${MAIN_ADOC_VALUE}.html" ]] ; then
-                ln -s ${ROOT_FOLDER}/${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
+                ln -s ${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
                 git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
             fi
             git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/$file
