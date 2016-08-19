2f93bd955a3399224a3cef5d799064342a28e0e7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-19 13:25:12 +0200

    Added docs on how to provide custom serviceName
    without this entry in the docs people got confused on how to do this.
    
    fixes #346

--- spring-cloud-sleuth-zipkin/src/test/java/org/springframework/cloud/sleuth/zipkin/HttpZipkinSpanReporterTest.java
+++ spring-cloud-sleuth-zipkin/src/test/java/org/springframework/cloud/sleuth/zipkin/HttpZipkinSpanReporterTest.java
@@ -1,9 +1,19 @@
 package org.springframework.cloud.sleuth.zipkin;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.cloud.sleuth.DefaultSpanNamer;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.log.NoOpSpanLogger;
 import org.springframework.cloud.sleuth.metric.CounterServiceBasedSpanMetricReporter;
 import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.cloud.sleuth.trace.DefaultTracer;
+import org.springframework.cloud.sleuth.util.ExceptionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import zipkin.Span;
@@ -12,6 +22,7 @@ import zipkin.junit.ZipkinRule;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
 
 public class HttpZipkinSpanReporterTest {
 
@@ -87,7 +98,7 @@ public class HttpZipkinSpanReporterTest {
 		);
 	}
 
-	@Test 
+	@Test
 	public void incrementsDroppedSpansWhenServerErrors() throws Exception {
 		this.zipkin.enqueueFailure(HttpFailure.sendErrorResponse(500, "Ouch"));
 
@@ -109,6 +120,36 @@ public class HttpZipkinSpanReporterTest {
 		this.reporter.flush(); // manually flush the spans
 
 		assertThat(this.inMemorySpanCounter.getDroppedSpans()).isEqualTo(2);
+	}
+
+	@Test
+	public void should_change_the_service_name_in_zipkin_to_the_manually_provided_one() {
+		AtomicReference<Span> receivedSpan = new AtomicReference<>();
+		Tracer tracer = new DefaultTracer(new AlwaysSampler(), new Random(), new DefaultSpanNamer(),
+				new NoOpSpanLogger(), new ZipkinSpanListener(receivedSpan::set,
+				new ServerPropertiesEndpointLocator(new ServerProperties(), "foo")));
+		// tag::service_name[]
+		org.springframework.cloud.sleuth.Span newSpan = tracer.createSpan("redis");
+		try {
+			newSpan.tag("redis.op", "get");
+			newSpan.tag("lc", "redis");
+			newSpan.logEvent(org.springframework.cloud.sleuth.Span.CLIENT_SEND);
+			// call redis service e.g
+			// return (SomeObj) redisTemplate.opsForHash().get("MYHASH", someObjKey);
+		} finally {
+			newSpan.tag("peer.service", "redisService");
+			newSpan.tag("peer.ipv4", "1.2.3.4");
+			newSpan.tag("peer.port", "1234");
+			newSpan.logEvent(org.springframework.cloud.sleuth.Span.CLIENT_RECV);
+			tracer.close(newSpan);
+		}
+		// end::service_name[]
+
+		then(tracer.getCurrentSpan()).isNull();
+		then(ExceptionUtils.getLastException()).isNull();
+		then(receivedSpan.get().binaryAnnotations)
+				.flatExtracting(input -> input.key, input -> new String(input.value))
+				.contains("peer.service", "redisService");
 	}
 
 	static Span span(long traceId, String spanName) {
