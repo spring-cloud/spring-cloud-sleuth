ea05343137eba4463146609ff6a877636eef1a55
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-08 13:03:40 +0200

    Fixed wrapping the wrapper

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/feign/TraceFeignObjectWrapper.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/feign/TraceFeignObjectWrapper.java
@@ -1,6 +1,9 @@
 package org.springframework.cloud.sleuth.instrument.web.client.feign;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.cloud.netflix.feign.ribbon.CachingSpringLoadBalancerFactory;
+import org.springframework.cloud.netflix.feign.ribbon.LoadBalancerFeignClient;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 
 import feign.Client;
 
@@ -14,14 +17,41 @@ final class TraceFeignObjectWrapper {
 
 	private final BeanFactory beanFactory;
 
+	private CachingSpringLoadBalancerFactory cachingSpringLoadBalancerFactory;
+	private SpringClientFactory springClientFactory;
+
 	TraceFeignObjectWrapper(BeanFactory beanFactory) {
 		this.beanFactory = beanFactory;
 	}
 
 	Object wrap(Object bean) {
 		if (bean instanceof Client && !(bean instanceof TraceFeignClient)) {
+			if (bean instanceof LoadBalancerFeignClient && !(bean instanceof TraceLoadBalancerFeignClient)) {
+				LoadBalancerFeignClient client = ((LoadBalancerFeignClient) bean);
+				return new TraceLoadBalancerFeignClient(
+						client.getDelegate(), factory(),
+						clientFactory(), this.beanFactory);
+			} else if (bean instanceof TraceLoadBalancerFeignClient) {
+				return bean;
+			}
 			return new TraceFeignClient(this.beanFactory, (Client) bean);
 		}
 		return bean;
+	}
+
+	private CachingSpringLoadBalancerFactory factory() {
+		if (this.cachingSpringLoadBalancerFactory == null) {
+			this.cachingSpringLoadBalancerFactory = this.beanFactory
+					.getBean(CachingSpringLoadBalancerFactory.class);
+		}
+		return this.cachingSpringLoadBalancerFactory;
+	}
+
+	private SpringClientFactory clientFactory() {
+		if (this.springClientFactory == null) {
+			this.springClientFactory = this.beanFactory
+					.getBean(SpringClientFactory.class);
+		}
+		return this.springClientFactory;
 	}
 }
