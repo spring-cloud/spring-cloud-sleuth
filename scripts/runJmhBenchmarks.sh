49ccfb57660e70b8c3830b95a28049ea4335e9d5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-02-27 17:00:41 +0100

    Trying to fix benchmarks

--- scripts/runJmhBenchmarks.sh
+++ scripts/runJmhBenchmarks.sh
@@ -2,4 +2,4 @@
 
 echo "Running JMH Benchmarks"
 ./mvnw clean install -DskipTests --projects benchmarks --also-make -Pbenchmarks,jmh
-java -Djmh.ignoreLock=true -jar benchmarks/target/benchmarks.jar org.springframework.cloud.sleuth.benchmarks.jmh.* -rf csv -rff target/jmh-result.csv | tee target/benchmarks.log
+java -Djmh.ignoreLock=true -jar benchmarks/target/benchmarks.jar org.springframework.cloud.sleuth.benchmarks.jmh.* -rf csv -rff jmh-result.csv | tee target/benchmarks.log
