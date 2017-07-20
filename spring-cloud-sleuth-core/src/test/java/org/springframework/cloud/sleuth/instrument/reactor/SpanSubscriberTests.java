2ebba99c4ef87e207fc611e1a3a485e374d23e57
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-20 11:58:10 +0200

    Reverting the change

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscriberTests.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscriberTests.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.awaitility.Awaitility;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,12 +21,8 @@ import org.springframework.cloud.sleuth.util.ExceptionUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Hooks;
 import reactor.core.scheduler.Schedulers;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -61,71 +58,7 @@ public class SpanSubscriberTests {
 				.subscribe(System.out::println);
 
 		then(this.tracer.getCurrentSpan()).isNull();
-		then(spanInOperation.get().getParents().get(0)).isEqualTo(span.getSpanId());
-		then(ExceptionUtils.getLastException()).isNull();
-	}
-
-	@Test public void should_support_reactor_fusion_optimization() {
-		Span span = this.tracer.createSpan("foo");
-		final AtomicReference<Span> spanInOperation = new AtomicReference<>();
-		log.info("Hello");
-
-		Mono.just(1)
-		    .flatMap( d -> Flux.just(d + 1).collectList().map(p -> p.get(0)))
-		    .map( d -> d + 1)
-		    .map( (d) -> {
-					spanInOperation.set(SpanSubscriberTests.this.tracer.getCurrentSpan());
-					return d + 1;
-				})
-		    .map( d -> d + 1)
-		    .subscribe(System.out::println);
-
-		then(this.tracer.getCurrentSpan()).isNull();
-		then(spanInOperation.get().getParents().get(0)).isEqualTo(span.getSpanId());
-		then(ExceptionUtils.getLastException()).isNull();
-	}
-
-	@Test public void should_not_trace_scalar_flows() {
-		Span span = this.tracer.createSpan("foo");
-		final AtomicReference<Subscription> spanInOperation = new AtomicReference<>();
-		log.info("Hello");
-
-		Mono.just(1)
-		    .subscribe(new BaseSubscriber<Integer>() {
-			    @Override
-			    protected void hookOnSubscribe(Subscription subscription) {
-				    spanInOperation.set(subscription);
-			    }
-		    });
-
-		then(this.tracer.getCurrentSpan()).isNotNull();
-		then(spanInOperation.get()).isNotInstanceOf(SpanSubscriber.class);
-
-		Mono.<Integer>error(new Exception())
-		    .subscribe(new BaseSubscriber<Integer>() {
-			    @Override
-			    protected void hookOnSubscribe(Subscription subscription) {
-				    spanInOperation.set(subscription);
-			    }
-
-			    @Override
-			    protected void hookOnError(Throwable throwable) {
-			    }
-		    });
-
-		then(this.tracer.getCurrentSpan()).isNotNull();
-		then(spanInOperation.get()).isNotInstanceOf(SpanSubscriber.class);
-
-		Mono.<Integer>empty()
-		    .subscribe(new BaseSubscriber<Integer>() {
-			    @Override
-			    protected void hookOnSubscribe(Subscription subscription) {
-				    spanInOperation.set(subscription);
-			    }
-		    });
-
-		then(this.tracer.getCurrentSpan()).isNotNull();
-		then(spanInOperation.get()).isNotInstanceOf(SpanSubscriber.class);
+		then(spanInOperation.get().getTraceId()).isEqualTo(span.getTraceId());
 		then(ExceptionUtils.getLastException()).isNull();
 	}
 
@@ -174,8 +107,14 @@ public class SpanSubscriberTests {
 		then(this.tracer.getCurrentSpan()).isEqualTo(foo2);
 		then(ExceptionUtils.getLastException()).isNull();
 		// parent cause there's an async span in the meantime
-		then(spanInOperation.get().getSavedSpan().getParents().get(0)).isEqualTo(foo2.getSpanId());
+		then(spanInOperation.get().getTraceId()).isEqualTo(foo2.getTraceId());
 		tracer.close(foo2);
+	}
+
+	@AfterClass
+	public static void cleanup() {
+		Hooks.resetOnNewSubscriber();
+		Schedulers.resetFactory();
 	}
 
 	@EnableAutoConfiguration
