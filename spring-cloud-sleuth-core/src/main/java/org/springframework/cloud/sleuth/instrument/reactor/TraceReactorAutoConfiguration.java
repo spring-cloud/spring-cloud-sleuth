fbbbc7e02c1162928d94ecb53286e62b739dfc48
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-21 11:17:19 +0200

    WIP

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/TraceReactorAutoConfiguration.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/TraceReactorAutoConfiguration.java
@@ -8,14 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.sleuth.SpanNamer;
 import org.springframework.cloud.sleuth.TraceKeys;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
 import org.springframework.cloud.sleuth.instrument.async.TraceableScheduledExecutorService;
-import org.springframework.cloud.sleuth.instrument.web.TraceWebFluxAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+
+import reactor.core.Fuseable;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -31,12 +32,11 @@ import reactor.core.scheduler.Schedulers;
 @Configuration
 @ConditionalOnProperty(value="spring.sleuth.reactor.enabled", matchIfMissing=true)
 @ConditionalOnClass(Mono.class)
-@AutoConfigureAfter(TraceWebFluxAutoConfiguration.class)
+@AutoConfigureAfter(TraceAutoConfiguration.class)
 public class TraceReactorAutoConfiguration {
 
 	@Configuration
 	@ConditionalOnBean(Tracer.class)
-	@ConditionalOnNotWebApplication
 	static class TraceReactorConfiguration {
 		@Autowired Tracer tracer;
 		@Autowired TraceKeys traceKeys;
@@ -44,8 +44,15 @@ public class TraceReactorAutoConfiguration {
 
 		@PostConstruct
 		public void setupHooks() {
-			Hooks.onNewSubscriber((pub, sub) ->
-					new SpanSubscriber(sub, sub.currentContext(), this.tracer, pub.toString()));
+			Hooks.onNewSubscriber((pub, sub) -> {
+				//do not trace fused flows or simple just/error/empty
+				if(pub instanceof Fuseable && sub instanceof Fuseable.QueueSubscription
+						|| pub instanceof Fuseable.ScalarCallable){
+					return sub;
+				}
+				return new SpanSubscriber(sub, sub.currentContext(), this.tracer, pub
+						.toString());
+			});
 			Schedulers.setFactory(new Schedulers.Factory() {
 				@Override public ScheduledExecutorService decorateScheduledExecutorService(
 						String schedulerType,
