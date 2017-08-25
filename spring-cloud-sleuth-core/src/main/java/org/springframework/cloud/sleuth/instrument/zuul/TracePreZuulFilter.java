17e303a1f7e8992338064547c67f6dc12cd4b015
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-08-25 13:07:36 +0200

    Removed remaining deprecations

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/zuul/TracePreZuulFilter.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/zuul/TracePreZuulFilter.java
@@ -16,6 +16,9 @@
 
 package org.springframework.cloud.sleuth.instrument.zuul;
 
+import java.lang.invoke.MethodHandles;
+import java.net.URI;
+
 import com.netflix.zuul.ExecutionStatus;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.ZuulFilterResult;
@@ -23,16 +26,12 @@ import com.netflix.zuul.context.RequestContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.sleuth.ErrorParser;
-import org.springframework.cloud.sleuth.ExceptionMessageErrorParser;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.cloud.sleuth.instrument.web.HttpSpanInjector;
 import org.springframework.cloud.sleuth.instrument.web.HttpTraceKeysInjector;
 import org.springframework.cloud.sleuth.instrument.web.TraceFilter;
 import org.springframework.cloud.sleuth.instrument.web.TraceRequestAttributes;
-
-import java.lang.invoke.MethodHandles;
-import java.net.URI;
 
 /**
  * A pre request {@link ZuulFilter} that sets tracing related headers on the request
@@ -56,15 +55,6 @@ public class TracePreZuulFilter extends ZuulFilter {
 	private final HttpSpanInjector spanInjector;
 	private final HttpTraceKeysInjector httpTraceKeysInjector;
 	private final ErrorParser errorParser;
-
-	@Deprecated
-	public TracePreZuulFilter(Tracer tracer, HttpSpanInjector spanInjector,
-			HttpTraceKeysInjector httpTraceKeysInjector) {
-		this.tracer = tracer;
-		this.spanInjector = spanInjector;
-		this.httpTraceKeysInjector = httpTraceKeysInjector;
-		this.errorParser = new ExceptionMessageErrorParser();
-	}
 
 	public TracePreZuulFilter(Tracer tracer, HttpSpanInjector spanInjector,
 			HttpTraceKeysInjector httpTraceKeysInjector, ErrorParser errorParser) {
