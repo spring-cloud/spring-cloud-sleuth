d7895519dd4d7f914fbad845fc76dfdf09028a03
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-14 11:32:16 +0100

    Removed type target from New/ContinueSpan; fixes #772

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/annotation/ContinueSpan.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/annotation/ContinueSpan.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@Target(value = { ElementType.METHOD, ElementType.TYPE })
+@Target(value = { ElementType.METHOD })
 public @interface ContinueSpan {
 
 	/**
