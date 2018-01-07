b60a309b3d07b2b723173b3d392a433c662ebe1b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-07 14:49:34 +0100

    Added annotation

---
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/brave/annotation/SleuthAdvisorConfig.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.brave.annotation;
+
+import javax.annotation.PostConstruct;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import brave.Span;
+import brave.Tracer;
+import brave.Tracing;
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.IntroductionInterceptor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.aop.support.DynamicMethodMatcherPointcut;
+import org.springframework.aop.support.annotation.AnnotationClassFilter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.cloud.brave.ErrorParser;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * Custom pointcut advisor that picks all classes / interfaces that
+ * have the Sleuth related annotations.
+ *
+ * @author Marcin Grzejszczak
+ * @since 1.2.0
+ */
+@SuppressWarnings("serial")
+class SleuthAdvisorConfig  extends AbstractPointcutAdvisor implements BeanFactoryAware {
+
+	private Advice advice;
+
+	private Pointcut pointcut;
+
+	private BeanFactory beanFactory;
+
+	@PostConstruct
+	public void init() {
+		this.pointcut = buildPointcut();
+		this.advice = buildAdvice();
+		if (this.advice instanceof BeanFactoryAware) {
+			((BeanFactoryAware) this.advice).setBeanFactory(this.beanFactory);
+		}
+	}
+
+	/**
+	 * Set the {@code BeanFactory} to be used when looking up executors by qualifier.
+	 */
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) {
+		this.beanFactory = beanFactory;
+	}
+
+	@Override
+	public Advice getAdvice() {
+		return this.advice;
+	}
+
+	@Override
+	public Pointcut getPointcut() {
+		return this.pointcut;
+	}
+
+	private Advice buildAdvice() {
+		return new SleuthInterceptor();
+	}
+
+	private Pointcut buildPointcut() {
+		return new AnnotationClassOrMethodOrArgsPointcut();
+	}
+
+	/**
+	 * Checks if a class or a method is is annotated with Sleuth related annotations
+	 */
+	private final class AnnotationClassOrMethodOrArgsPointcut extends
+			DynamicMethodMatcherPointcut {
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass, Object... args) {
+			return getClassFilter().matches(targetClass);
+		}
+
+		@Override public ClassFilter getClassFilter() {
+			return new ClassFilter() {
+				@Override public boolean matches(Class<?> clazz) {
+					return new AnnotationClassOrMethodFilter(NewSpan.class).matches(clazz) ||
+							new AnnotationClassOrMethodFilter(ContinueSpan.class).matches(clazz);
+				}
+			};
+		}
+
+	}
+
+	private final class AnnotationClassOrMethodFilter extends AnnotationClassFilter {
+
+		private final AnnotationMethodsResolver methodResolver;
+
+		AnnotationClassOrMethodFilter(Class<? extends Annotation> annotationType) {
+			super(annotationType, true);
+			this.methodResolver = new AnnotationMethodsResolver(annotationType);
+		}
+
+		@Override
+		public boolean matches(Class<?> clazz) {
+			return super.matches(clazz) || this.methodResolver.hasAnnotatedMethods(clazz);
+		}
+
+	}
+
+	/**
+	 * Checks if a method is properly annotated with a given Sleuth annotation
+	 */
+	private static class AnnotationMethodsResolver {
+
+		private final Class<? extends Annotation> annotationType;
+
+		public AnnotationMethodsResolver(Class<? extends Annotation> annotationType) {
+			this.annotationType = annotationType;
+		}
+
+		public boolean hasAnnotatedMethods(Class<?> clazz) {
+			final AtomicBoolean found = new AtomicBoolean(false);
+			ReflectionUtils.doWithMethods(clazz,
+					new ReflectionUtils.MethodCallback() {
+						@Override
+						public void doWith(Method method) throws IllegalArgumentException,
+								IllegalAccessException {
+							if (found.get()) {
+								return;
+							}
+							Annotation annotation = AnnotationUtils.findAnnotation(method,
+									SleuthAdvisorConfig.AnnotationMethodsResolver.this.annotationType);
+							if (annotation != null) { found.set(true); }
+						}
+					});
+			return found.get();
+		}
+
+	}
+}
+
+/**
+ * Interceptor that creates or continues a span depending on the provided
+ * annotation. Also it adds logs and tags if necessary.
+ */
+class SleuthInterceptor implements IntroductionInterceptor, BeanFactoryAware  {
+
+	private static final Log logger = LogFactory.getLog(SleuthInterceptor.class);
+	private static final String CLASS_KEY = "class";
+	private static final String METHOD_KEY = "method";
+
+	private BeanFactory beanFactory;
+	private SpanCreator spanCreator;
+	private Tracing tracing;
+	private SpanTagAnnotationHandler spanTagAnnotationHandler;
+	private ErrorParser errorParser;
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		Method method = invocation.getMethod();
+		if (method == null) {
+			return invocation.proceed();
+		}
+		Method mostSpecificMethod = AopUtils
+				.getMostSpecificMethod(method, invocation.getThis().getClass());
+		NewSpan newSpan = SleuthAnnotationUtils.findAnnotation(mostSpecificMethod, NewSpan.class);
+		ContinueSpan continueSpan = SleuthAnnotationUtils.findAnnotation(mostSpecificMethod, ContinueSpan.class);
+		if (newSpan == null && continueSpan == null) {
+			return invocation.proceed();
+		}
+		Span span = tracing().tracer().currentSpan();
+		if (newSpan != null || span == null) {
+			span = spanCreator().createSpan(invocation, newSpan);
+		}
+		String log = log(continueSpan);
+		boolean hasLog = StringUtils.hasText(log);
+		try (Tracer.SpanInScope ws = tracing().tracer().withSpanInScope(span)) {
+			if (hasLog) {
+				logEvent(span, log + ".before");
+			}
+			spanTagAnnotationHandler().addAnnotatedParameters(invocation);
+			addTags(invocation, span);
+			return invocation.proceed();
+		} catch (Exception e) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Exception occurred while trying to continue the pointcut", e);
+			}
+			if (hasLog) {
+				logEvent(span, log + ".afterFailure");
+			}
+			errorParser().parseErrorTags(span, e);
+			throw e;
+		} finally {
+			if (span != null) {
+				if (hasLog) {
+					logEvent(span, log + ".after");
+				}
+				if (newSpan != null) {
+					span.finish();
+				}
+			}
+		}
+	}
+
+	private void addTags(MethodInvocation invocation, Span span) {
+		span.tag(CLASS_KEY, invocation.getThis().getClass().getSimpleName());
+		span.tag(METHOD_KEY, invocation.getMethod().getName());
+	}
+
+	private void logEvent(Span span, String name) {
+		if (span == null) {
+			logger.warn("You were trying to continue a span which was null. Please "
+					+ "remember that if two proxied methods are calling each other from "
+					+ "the same class then the aspect will not be properly resolved");
+			return;
+		}
+		span.annotate(name);
+	}
+
+	private String log(ContinueSpan continueSpan) {
+		if (continueSpan != null) {
+			return continueSpan.log();
+		}
+		return "";
+	}
+
+	private Tracing tracing() {
+		if (this.tracing == null) {
+			this.tracing = this.beanFactory.getBean(Tracing.class);
+		}
+		return this.tracing;
+	}
+
+	private SpanCreator spanCreator() {
+		if (this.spanCreator == null) {
+			this.spanCreator = this.beanFactory.getBean(SpanCreator.class);
+		}
+		return this.spanCreator;
+	}
+
+	private SpanTagAnnotationHandler spanTagAnnotationHandler() {
+		if (this.spanTagAnnotationHandler == null) {
+			this.spanTagAnnotationHandler = new SpanTagAnnotationHandler(this.beanFactory);
+		}
+		return this.spanTagAnnotationHandler;
+	}
+
+	private ErrorParser errorParser() {
+		if (this.errorParser == null) {
+			this.errorParser = this.beanFactory.getBean(ErrorParser.class);
+		}
+		return this.errorParser;
+	}
+
+	@Override public boolean implementsInterface(Class<?> intf) {
+		return true;
+	}
+
+	@Override public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
+}
---
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/brave/annotation/SpelTagValueExpressionResolverTests.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.brave.annotation;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+public class SpelTagValueExpressionResolverTests {
+	@Test
+	public void should_use_spel_to_resolve_a_value() throws Exception {
+		SpelTagValueExpressionResolver resolver = new SpelTagValueExpressionResolver();
+
+		String resolved = resolver.resolve("length() + 1", "foo");
+
+		then(resolved).isEqualTo("4");
+	}
+
+	@Test
+	public void should_use_to_string_if_expression_is_not_analyzed_properly() throws Exception {
+		SpelTagValueExpressionResolver resolver = new SpelTagValueExpressionResolver();
+
+		String resolved = resolver.resolve("invalid() structure + 1", new Foo());
+
+		then(resolved).isEqualTo("BAR");
+	}
+}
+
+class Foo {
+	@Override public String toString() {
+		return "BAR";
+	}
+}
