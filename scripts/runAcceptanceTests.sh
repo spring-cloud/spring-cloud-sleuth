5ddce43746f860dad443c60ae11eb8723e00d285
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-25 13:47:50 +0100

    Added branch checking

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -6,6 +6,7 @@ mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/runAcceptanceTests.sh"
 AT_WHAT_TO_TEST="SLEUTH"
+BRANCH_NAME="edgware"
 
 cd target
 
@@ -14,6 +15,6 @@ curl "${SCRIPT_URL}" --output runAcceptanceTests.sh
 chmod +x runAcceptanceTests.sh
 
 echo "Killing all running apps"
-./runAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -n
+./runAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -n -br "${BRANCH_NAME}"
 
-./runAcceptanceTests.sh --whattotest "${AT_WHAT_TO_TEST}" --killattheend
+./runAcceptanceTests.sh --whattotest "${AT_WHAT_TO_TEST}" --killattheend -br "${BRANCH_NAME}"
