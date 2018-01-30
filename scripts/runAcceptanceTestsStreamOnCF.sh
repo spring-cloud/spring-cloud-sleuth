d91a33901888587712c5bb283765f7f32195c1d7
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-30 18:17:32 +0100

    Changed type for cf tests

--- scripts/runAcceptanceTestsStreamOnCF.sh
+++ scripts/runAcceptanceTestsStreamOnCF.sh
@@ -5,7 +5,7 @@ set -o errexit
 mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/runAcceptanceTests.sh"
-AT_WHAT_TO_TEST="SLEUTH_STREAM"
+AT_WHAT_TO_TEST="SLEUTH"
 BRANCH_NAME="master"
 
 cd target
