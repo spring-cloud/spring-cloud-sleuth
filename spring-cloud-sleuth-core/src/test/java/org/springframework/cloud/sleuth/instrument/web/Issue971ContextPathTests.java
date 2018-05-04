42d17009be2f6f3792bdc48d66141ad3736093dc
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-05-04 07:40:05 -0400

    WIP, verify if bean is there for reactive apps

---
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/web/Issue971ContextPathTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.sleuth.instrument.web;
+
+import brave.Tracing;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.sleuth.util.ArrayListSpanReporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Issue971ContextPathTests.Config.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = {"spring.sleuth.sampler.probability:1",
+				"management.endpoints.web.exposure.include:*",
+				"server.servlet.context-path:/context-path"})
+public class Issue971ContextPathTests {
+
+	@Autowired ArrayListSpanReporter accumulator;
+
+	@LocalServerPort int port;
+
+	@Test
+	public void should_not_sample_skipped_endpoint_with_context_path() {
+		new RestTemplate().getForObject("http://localhost:" + port + "/context-path/actuator/health", String.class);
+		
+		then(Tracing.current().tracer().currentSpan()).isNull();
+		then(this.accumulator.getSpans()).hasSize(0); // this fails
+	}
+
+	@EnableAutoConfiguration
+	@Configuration
+	public static class Config {
+
+		@Bean ArrayListSpanReporter reporter() {
+			return new ArrayListSpanReporter();
+		}
+	}
+}
