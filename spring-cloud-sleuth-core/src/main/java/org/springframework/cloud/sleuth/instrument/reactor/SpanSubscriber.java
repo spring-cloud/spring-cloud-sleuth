f5c4a27f9adc6ea2d42d437c8b5fb6f009a2854b
Simon Baslé <sbasle@pivotal.io>
2018-06-13 19:01:22 +0200

    follow-up to #995 All SpanSubscription now fake QueueSubscription

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscriber.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscriber.java
@@ -36,32 +36,7 @@ import reactor.util.context.Context;
  * @author Marcin Grzejszczak
  * @since 2.0.0
  */
-final class SpanSubscriber<T> extends AtomicBoolean implements SpanSubscription<T>, Fuseable.QueueSubscription<T> {
-
-	@Override
-	public T poll() {
-		return null;
-	}
-
-	@Override
-	public int requestFusion(int i) {
-		return Fuseable.NONE; //always negotiate to no fusion
-	}
-
-	@Override
-	public int size() {
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return true;
-	}
-
-	@Override
-	public void clear() {
-		//NO-OP
-	}
+final class SpanSubscriber<T> extends AtomicBoolean implements SpanSubscription<T> {
 
 	private static final Logger log = Loggers.getLogger(
 			SpanSubscriber.class);
--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscription.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscription.java
@@ -18,9 +18,40 @@ package org.springframework.cloud.sleuth.instrument.reactor;
 
 import org.reactivestreams.Subscription;
 import reactor.core.CoreSubscriber;
+import reactor.core.Fuseable;
 
 /**
+ * A {@link SpanSubscription} is a {@link Subscription} that fakes being {@link Fuseable}
+ * (implementing {@link reactor.core.Fuseable.QueueSubscription} with default no-op methods
+ * and always negotiating fusion to be {@link Fuseable#NONE}).
+ *
  * @author Marcin Grzejszczak
  */
-interface SpanSubscription<T> extends Subscription, CoreSubscriber<T> {
+interface SpanSubscription<T> extends Subscription, CoreSubscriber<T>, Fuseable.QueueSubscription<T> {
+
+    @Override
+    default T poll() {
+        return null;
+    }
+
+    @Override
+    default int requestFusion(int i) {
+        return Fuseable.NONE; //always negotiate to no fusion
+    }
+
+    @Override
+    default int size() {
+        return 0;
+    }
+
+    @Override
+    default boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    default void clear() {
+        //NO-OP
+    }
+
 }
