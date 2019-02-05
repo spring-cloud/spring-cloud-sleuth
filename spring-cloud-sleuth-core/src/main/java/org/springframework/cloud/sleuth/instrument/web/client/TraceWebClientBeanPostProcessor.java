spring-cloud/spring-cloud-sleuth

commit d3feae1ce4276b8a82c9ff958331dbad1071c55a

Stephane help (#1126)
* WIP : Optimize Reactor instrumentation
- Use onLastOperator instead of onEachOperator
- Do not instrument scalar publishers
- Revisit ReactorSleuthMethodInvocationProcessor to reduce ops overhead

* Optimize Reactor instrumentation
- Use only one operator for the MethodInvocationProcessor
- Warning Behavior Change: @NewSpan will defer Span creation/context

* Optimize Reactor Optimization
- Reduce WebFilter instrumentation to 1 operator
- Reduce WebClient instrumentation to 2 operators (1 resp, 1 body)

* Late Formatting commit - because not enough coffee

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/TraceWebClientBeanPostProcessor.java
@@ -19,20 +19,29 @@ package org.springframework.cloud.sleuth.instrument.web.client;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import brave.Span;
 import brave.Tracer;
+import brave.Tracing;
 import brave.http.HttpClientHandler;
 import brave.http.HttpTracing;
 import brave.propagation.Propagation;
 import brave.propagation.TraceContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscription;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.Nullable;
+import reactor.util.context.Context;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cloud.sleuth.instrument.reactor.ReactorSleuth;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -47,7 +56,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @author Marcin Grzejszczak
  * @since 2.0.0
  */
-class TraceWebClientBeanPostProcessor implements BeanPostProcessor {
+final class TraceWebClientBeanPostProcessor implements BeanPostProcessor {
 
 	private final BeanFactory beanFactory;
 
@@ -87,7 +96,7 @@ class TraceWebClientBeanPostProcessor implements BeanPostProcessor {
 
 }
 
-class TraceExchangeFilterFunction implements ExchangeFilterFunction {
+final class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 
 	private static final Log log = LogFactory.getLog(TraceExchangeFilterFunction.class);
 
@@ -117,6 +126,8 @@ class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 
 	final BeanFactory beanFactory;
 
+	final Function<? super Publisher<DataBuffer>, ? extends Publisher<DataBuffer>> scopePassingTransformer;
+
 	Tracer tracer;
 
 	HttpTracing httpTracing;
@@ -127,86 +138,185 @@ class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 
 	TraceExchangeFilterFunction(BeanFactory beanFactory) {
 		this.beanFactory = beanFactory;
+		this.scopePassingTransformer = ReactorSleuth
+				.scopePassingSpanOperator(beanFactory);
 	}
 
 	@Override
 	public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
-		final ClientRequest.Builder builder = ClientRequest.from(request);
-		Mono<ClientResponse> exchange = Mono.defer(() -> next.exchange(builder.build()))
-				.cast(Object.class).onErrorResume(Mono::just)
-				.zipWith(Mono.subscriberContext()).flatMap(anyAndContext -> {
-					if (log.isDebugEnabled()) {
-						log.debug("Wrapping the context [" + anyAndContext + "]");
-					}
-					Object any = anyAndContext.getT1();
-					Span clientSpan = anyAndContext.getT2().get(CLIENT_SPAN_KEY);
-					Mono<ClientResponse> continuation;
-					final Tracer.SpanInScope ws = tracer().withSpanInScope(clientSpan);
-					if (any instanceof Throwable) {
-						continuation = Mono.error((Throwable) any);
-					}
-					else {
-						continuation = Mono.just((ClientResponse) any);
-					}
-					return continuation
-							.doAfterSuccessOrError((clientResponse, throwable1) -> {
-								Throwable throwable = throwable1;
-								if (clientResponse == null
-										|| clientResponse.statusCode() == null) {
-									if (log.isDebugEnabled()) {
-										log.debug(
-												"No response was returned. Will close the span ["
-														+ clientSpan + "]");
-									}
-									handleReceive(clientSpan, ws, clientResponse,
-											throwable);
-									return;
-								}
-								boolean error = clientResponse.statusCode()
-										.is4xxClientError()
-										|| clientResponse.statusCode().is5xxServerError();
-								if (error) {
-									if (log.isDebugEnabled()) {
-										log.debug(
-												"Non positive status code was returned from the call. Will close the span ["
-														+ clientSpan + "]");
-									}
-									throwable = new RestClientException(
-											"Status code of the response is ["
-													+ clientResponse.statusCode().value()
-													+ "] and the reason is ["
-													+ clientResponse.statusCode()
-															.getReasonPhrase()
-													+ "]");
-								}
-								handleReceive(clientSpan, ws, clientResponse, throwable);
-							});
-				}).subscriberContext(c -> {
-					if (log.isDebugEnabled()) {
-						log.debug("Instrumenting WebClient call");
-					}
-					Span parent = c.getOrDefault(Span.class, null);
-					Span clientSpan = handler().handleSend(injector(), builder, request,
-							tracer().nextSpan());
-					if (log.isDebugEnabled()) {
-						log.debug("Handled send of " + clientSpan);
-					}
-					if (parent == null) {
-						c = c.put(Span.class, clientSpan);
-						if (log.isDebugEnabled()) {
-							log.debug("Reactor Context got injected with the client span "
-									+ clientSpan);
-						}
-					}
-					return c.put(CLIENT_SPAN_KEY, clientSpan);
-				});
-		return exchange;
+		return new MonoWebClientTrace(next, request, this);
 	}
 
-	private void handleReceive(Span clientSpan, Tracer.SpanInScope ws,
-			ClientResponse clientResponse, Throwable throwable) {
-		handler().handleReceive(clientResponse, throwable, clientSpan);
-		ws.close();
+	private static final class MonoWebClientTrace extends Mono<ClientResponse> {
+
+		final ExchangeFunction next;
+
+		final ClientRequest request;
+
+		final Tracer tracer;
+
+		final HttpClientHandler<ClientRequest, ClientResponse> handler;
+
+		final TraceContext.Injector<ClientRequest.Builder> injector;
+
+		final Tracing tracing;
+
+		final Function<? super Publisher<DataBuffer>, ? extends Publisher<DataBuffer>> scopePassingTransformer;
+
+		MonoWebClientTrace(ExchangeFunction next, ClientRequest request,
+				TraceExchangeFilterFunction parent) {
+			this.next = next;
+			this.request = request;
+			this.tracer = parent.tracer();
+			this.handler = parent.handler();
+			this.injector = parent.injector();
+			this.tracing = parent.httpTracing().tracing();
+			this.scopePassingTransformer = parent.scopePassingTransformer;
+		}
+
+		@Override
+		public void subscribe(CoreSubscriber<? super ClientResponse> subscriber) {
+			final ClientRequest.Builder builder = ClientRequest.from(this.request);
+
+			Context context = subscriber.currentContext();
+
+			this.next.exchange(builder.build()).subscribe(new WebClientTracerSubscriber(
+					subscriber, context, findOrCreateSpan(builder), this));
+		}
+
+		private Span findOrCreateSpan(ClientRequest.Builder builder) {
+			if (log.isDebugEnabled()) {
+				log.debug("Instrumenting WebClient call");
+			}
+			Span clientSpan = this.handler.handleSend(this.injector, builder,
+					this.request, this.tracer.nextSpan());
+			if (log.isDebugEnabled()) {
+				log.debug("Handled send of " + clientSpan);
+			}
+			return clientSpan;
+		}
+
+		static final class WebClientTracerSubscriber
+				implements CoreSubscriber<ClientResponse> {
+
+			final CoreSubscriber<? super ClientResponse> actual;
+
+			final Context context;
+
+			final Span span;
+
+			final Tracer.SpanInScope ws;
+
+			final HttpClientHandler<ClientRequest, ClientResponse> handler;
+
+			final Function<? super Publisher<DataBuffer>, ? extends Publisher<DataBuffer>> scopePassingTransformer;
+
+			final Tracing tracing;
+
+			boolean done;
+
+			WebClientTracerSubscriber(CoreSubscriber<? super ClientResponse> actual,
+					Context context, Span span, MonoWebClientTrace parent) {
+				this.actual = actual;
+				this.span = span;
+				this.handler = parent.handler;
+				this.tracing = parent.tracing;
+				this.scopePassingTransformer = parent.scopePassingTransformer;
+
+				if (!context.hasKey(Span.class)) {
+					context = context.put(Span.class, span);
+					if (log.isDebugEnabled()) {
+						log.debug("Reactor Context got injected with the client span "
+								+ span);
+					}
+				}
+
+				this.context = context.put(CLIENT_SPAN_KEY, span);
+				this.ws = parent.tracer.withSpanInScope(span);
+
+			}
+
+			@Override
+			public void onSubscribe(Subscription subscription) {
+				this.actual.onSubscribe(subscription);
+			}
+
+			@Override
+			public void onNext(ClientResponse response) {
+				done = true;
+				try {
+					// decorate response body
+					this.actual.onNext(ClientResponse.from(response)
+							.body(response.bodyToFlux(DataBuffer.class)
+									.transform(scopePassingTransformer))
+							.build());
+				}
+				finally {
+					terminateSpan(response, null);
+				}
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				try {
+					this.actual.onError(t);
+				}
+				finally {
+					terminateSpan(null, t);
+				}
+			}
+
+			@Override
+			public void onComplete() {
+				try {
+					this.actual.onComplete();
+				}
+				finally {
+					if (!done) {
+						terminateSpan(null, null);
+					}
+				}
+			}
+
+			@Override
+			public Context currentContext() {
+				return this.context;
+			}
+
+			void handleReceive(Span clientSpan, Tracer.SpanInScope ws,
+					ClientResponse clientResponse, Throwable throwable) {
+				this.handler.handleReceive(clientResponse, throwable, clientSpan);
+				ws.close();
+			}
+
+			void terminateSpan(@Nullable ClientResponse clientResponse,
+					@Nullable Throwable throwable) {
+				if (clientResponse == null || clientResponse.statusCode() == null) {
+					if (log.isDebugEnabled()) {
+						log.debug("No response was returned. Will close the span [" + span
+								+ "]");
+					}
+					handleReceive(span, ws, clientResponse, throwable);
+					return;
+				}
+				boolean error = clientResponse.statusCode().is4xxClientError()
+						|| clientResponse.statusCode().is5xxServerError();
+				if (error) {
+					if (log.isDebugEnabled()) {
+						log.debug(
+								"Non positive status code was returned from the call. Will close the span ["
+										+ span + "]");
+					}
+					throwable = new RestClientException("Status code of the response is ["
+							+ clientResponse.statusCode().value()
+							+ "] and the reason is ["
+							+ clientResponse.statusCode().getReasonPhrase() + "]");
+				}
+				handleReceive(span, ws, clientResponse, throwable);
+			}
+
+		}
+
 	}
 
 	@SuppressWarnings("unchecked")

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/annotation/ReactorSleuthMethodInvocationProcessor.java
@@ -28,7 +28,9 @@ import org.reactivestreams.Subscription;
 import reactor.core.CoreSubscriber;
 import reactor.core.Scannable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxOperator;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoOperator;
 import reactor.util.annotation.Nullable;
 import reactor.util.context.Context;
 
@@ -85,43 +87,37 @@ class ReactorSleuthMethodInvocationProcessor
 		Publisher<?> publisher = (Publisher) invocation.proceed();
 
 		if (publisher instanceof Mono) {
-			return new MonoSpan((Mono<Object>) publisher,
-					this,
-					newSpan,
-					span,
-					invocation,
+			return new MonoSpan((Mono<Object>) publisher, this, newSpan, span, invocation,
 					log);
 		}
 		else if (publisher instanceof Flux) {
-			return new FluxSpan((Flux<Object>) publisher,
-					this,
-					newSpan,
-					span,
-					invocation,
+			return new FluxSpan((Flux<Object>) publisher, this, newSpan, span, invocation,
 					log);
 		}
 		else {
-			throw new IllegalArgumentException("Unexpected type of publisher: " + publisher.getClass());
+			throw new IllegalArgumentException(
+					"Unexpected type of publisher: " + publisher.getClass());
 		}
 	}
 
-	private static final class FluxSpan extends Flux<Object> implements Scannable {
+	private static final class FluxSpan extends FluxOperator<Object, Object> {
 
-		final Flux<Object>                           source;
-		final Span                                   span;
-		final MethodInvocation                       invocation;
-		final String                                 log;
-		final boolean                                hasLog;
+		final Span span;
+
+		final MethodInvocation invocation;
+
+		final String log;
+
+		final boolean hasLog;
+
 		final ReactorSleuthMethodInvocationProcessor processor;
-		final NewSpan                                newSpan;
 
-		FluxSpan(Flux<Object> source,
-				ReactorSleuthMethodInvocationProcessor processor,
-				NewSpan newSpan,
-				@Nullable Span span,
-				MethodInvocation invocation,
+		final NewSpan newSpan;
+
+		FluxSpan(Flux<Object> source, ReactorSleuthMethodInvocationProcessor processor,
+				NewSpan newSpan, @Nullable Span span, MethodInvocation invocation,
 				String log) {
-			this.source = source;
+			super(source);
 			this.span = span;
 			this.newSpan = newSpan;
 			this.invocation = invocation;
@@ -143,44 +139,31 @@ class ReactorSleuthMethodInvocationProcessor
 				span = this.span;
 			}
 			try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
-				this.source.subscribe(new SpanSubscriber(actual,
-						this.processor,
-						this.invocation,
-						this.span == null,
-						span,
-						this.log,
-						this.hasLog));
+				this.source.subscribe(new SpanSubscriber(actual, this.processor,
+						this.invocation, this.span == null, span, this.log, this.hasLog));
 			}
 		}
 
-		@Nullable
-		public Object scanUnsafe(Attr key) {
-			if (key == Attr.PARENT) {
-				return this.source;
-			}
-			else {
-				return null;
-			}
-		}
 	}
 
-	private static final class MonoSpan extends Mono<Object> implements Scannable {
+	private static final class MonoSpan extends MonoOperator<Object, Object> {
 
-		final Mono<Object>                           source;
-		final Span                                   span;
-		final MethodInvocation                       invocation;
-		final String                                 log;
-		final boolean                                hasLog;
+		final Span span;
+
+		final MethodInvocation invocation;
+
+		final String log;
+
+		final boolean hasLog;
+
 		final ReactorSleuthMethodInvocationProcessor processor;
-		final NewSpan                                newSpan;
 
-		MonoSpan(Mono<Object> source,
-				ReactorSleuthMethodInvocationProcessor processor,
-				NewSpan newSpan,
-				@Nullable Span span,
-				MethodInvocation invocation,
+		final NewSpan newSpan;
+
+		MonoSpan(Mono<Object> source, ReactorSleuthMethodInvocationProcessor processor,
+				NewSpan newSpan, @Nullable Span span, MethodInvocation invocation,
 				String log) {
-			this.source = source;
+			super(source);
 			this.processor = processor;
 			this.newSpan = newSpan;
 			this.span = span;
@@ -202,48 +185,37 @@ class ReactorSleuthMethodInvocationProcessor
 				span = this.span;
 			}
 			try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
-				this.source.subscribe(new SpanSubscriber(actual,
-						this.processor,
-						this.invocation,
-						this.span == null,
-						span,
-						this.log,
-						this.hasLog));
+				this.source.subscribe(new SpanSubscriber(actual, this.processor,
+						this.invocation, this.span == null, span, this.log, this.hasLog));
 			}
 		}
 
-		@Nullable
-		public Object scanUnsafe(Attr key) {
-			if (key == Attr.PARENT) {
-				return this.source;
-			}
-			else {
-				return null;
-			}
-		}
 	}
 
-	private static final class SpanSubscriber implements CoreSubscriber<Object>,
-	                                                     Subscription,
-	                                                     Scannable {
+	private static final class SpanSubscriber
+			implements CoreSubscriber<Object>, Subscription, Scannable {
 
-		final CoreSubscriber<? super Object>         actual;
-		final boolean                                isNewSpan;
-		final Span                                   span;
-		final String                                 log;
-		final boolean                                hasLog;
-		final CurrentTraceContext                    currentTraceContext;
+		final CoreSubscriber<? super Object> actual;
+
+		final boolean isNewSpan;
+
+		final Span span;
+
+		final String log;
+
+		final boolean hasLog;
+
+		final CurrentTraceContext currentTraceContext;
+
 		final ReactorSleuthMethodInvocationProcessor processor;
-		final Context                                context;
+
+		final Context context;
 
 		Subscription parent;
 
 		SpanSubscriber(CoreSubscriber<? super Object> actual,
 				ReactorSleuthMethodInvocationProcessor processor,
-				MethodInvocation invocation,
-				boolean isNewSpan,
-				Span span,
-				String log,
+				MethodInvocation invocation, boolean isNewSpan, Span span, String log,
 				boolean hasLog) {
 			this.actual = actual;
 			this.isNewSpan = isNewSpan;
@@ -332,6 +304,7 @@ class ReactorSleuthMethodInvocationProcessor
 			}
 			return null;
 		}
+
 	}
 
 	private boolean isReactorReturnType(Class<?> returnType) {

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/ReactorSleuth.java
@@ -64,14 +64,12 @@ public abstract class ReactorSleuth {
 			log.trace("Scope passing operator [" + beanFactory + "]");
 		}
 
-		//Adapt if lazy bean factory
-		BooleanSupplier isActive =
-				beanFactory instanceof ConfigurableApplicationContext ?
-						((ConfigurableApplicationContext) beanFactory)::isActive :
-						() -> true;
+		// Adapt if lazy bean factory
+		BooleanSupplier isActive = beanFactory instanceof ConfigurableApplicationContext
+				? ((ConfigurableApplicationContext) beanFactory)::isActive : () -> true;
 
 		return Operators.liftPublisher((p, sub) -> {
-			//if Flux/Mono #just, #empty, #error
+			// if Flux/Mono #just, #empty, #error
 			if (p instanceof Fuseable.ScalarCallable) {
 				return sub;
 			}
@@ -81,40 +79,43 @@ public abstract class ReactorSleuth {
 				if (log.isTraceEnabled()) {
 					log.trace("Spring Context [" + beanFactory
 							+ "] already refreshed. Creating a scope "
-							+ "passing span subscriber with Reactor Context "
-							+ "[" + sub.currentContext() + "] and name ["
-							+ scannable.name() + "]");
+							+ "passing span subscriber with Reactor Context " + "["
+							+ sub.currentContext() + "] and name [" + scannable.name()
+							+ "]");
 				}
 
-				return scopePassingSpanSubscription(beanFactory.getBean(Tracing.class), sub);
+				return scopePassingSpanSubscription(beanFactory.getBean(Tracing.class),
+						sub);
 			}
 			if (log.isTraceEnabled()) {
 				log.trace("Spring Context [" + beanFactory
 						+ "] is not yet refreshed, falling back to lazy span subscriber. Reactor Context is ["
-						+ sub.currentContext() + "] and name is ["
-						+ scannable.name() + "]");
+						+ sub.currentContext() + "] and name is [" + scannable.name()
+						+ "]");
 			}
-			return new LazySpanSubscriber<>(lazyScopePassingSpanSubscription(beanFactory, scannable, sub));
+			return new LazySpanSubscriber<>(
+					lazyScopePassingSpanSubscription(beanFactory, scannable, sub));
 		});
 	}
 
 	static <T> SpanSubscriptionProvider<T> lazyScopePassingSpanSubscription(
 			BeanFactory beanFactory, Scannable scannable, CoreSubscriber<? super T> sub) {
-		return new SpanSubscriptionProvider<>(beanFactory, sub, sub.currentContext(), scannable.name());
+		return new SpanSubscriptionProvider<>(beanFactory, sub, sub.currentContext(),
+				scannable.name());
 	}
 
-
-	static <T> CoreSubscriber<? super T> scopePassingSpanSubscription(
-			Tracing tracing, CoreSubscriber<? super T> sub) {
+	static <T> CoreSubscriber<? super T> scopePassingSpanSubscription(Tracing tracing,
+			CoreSubscriber<? super T> sub) {
 
 		Context context = sub.currentContext();
 
-		Span root = context.hasKey(Span.class) ? context.get(Span.class) : tracing.tracer().currentSpan();
+		Span root = context.hasKey(Span.class) ? context.get(Span.class)
+				: tracing.tracer().currentSpan();
 		if (root != null) {
 			return new ScopePassingSpanSubscriber<>(sub, context, tracing, root);
 		}
 		else {
-			return sub; //no need to trace
+			return sub; // no need to trace
 		}
 	}
 

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/ScopePassingSpanSubscriber.java
@@ -123,8 +123,10 @@ final class ScopePassingSpanSubscriber<T> implements SpanSubscription<T>, Scanna
 	public Object scanUnsafe(Attr key) {
 		if (key == Attr.PARENT) {
 			return this.s;
-		} else {
+		}
+		else {
 			return key == Attr.ACTUAL ? this.subscriber : null;
 		}
 	}
+
 }

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/reactor/SpanSubscriptionProvider.java
@@ -64,8 +64,10 @@ final class SpanSubscriptionProvider<T> implements Supplier<SpanSubscription<T>>
 	}
 
 	SpanSubscription<T> newCoreSubscriber(Tracing tracing) {
-		Span root = context.hasKey(Span.class) ? context.get(Span.class) : tracing.tracer().currentSpan();
-		return new ScopePassingSpanSubscriber<>(this.subscriber, this.context, tracing, root);
+		Span root = context.hasKey(Span.class) ? context.get(Span.class)
+				: tracing.tracer().currentSpan();
+		return new ScopePassingSpanSubscriber<>(this.subscriber, this.context, tracing,
+				root);
 	}
 
 	private Tracing tracing() {

commit 7e56dc67ebdfd1de7de71a26584313eadd97ce65

Fix regression introduced in #1126 (brave headers propagation)
Before #1126, the headers were eagerly set in
`TraceExchangeFilterFunction#filter`. After it, the side effect was
moved to lazy `MonoWebClientTrace#subscribe`.

However, we have everything to instrument the request in `filter`,
and it can be done eagerly.

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/TraceWebClientBeanPostProcessor.java
@@ -146,7 +146,17 @@ final class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 
 	@Override
 	public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
-		return new MonoWebClientTrace(next, request, this);
+		ClientRequest.Builder builder = ClientRequest.from(request);
+		if (log.isDebugEnabled()) {
+			log.debug("Instrumenting WebClient call");
+		}
+		Span span = handler().handleSend(injector(), builder, request,
+				tracer().nextSpan());
+		if (log.isDebugEnabled()) {
+			log.debug("Handled send of " + span);
+		}
+
+		return new MonoWebClientTrace(next, builder.build(), this, span);
 	}
 
 	private static final class MonoWebClientTrace extends Mono<ClientResponse> {
@@ -165,8 +175,10 @@ final class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 
 		final Function<? super Publisher<DataBuffer>, ? extends Publisher<DataBuffer>> scopePassingTransformer;
 
+		private final Span span;
+
 		MonoWebClientTrace(ExchangeFunction next, ClientRequest request,
-				TraceExchangeFilterFunction parent) {
+				TraceExchangeFilterFunction parent, Span span) {
 			this.next = next;
 			this.request = request;
 			this.tracer = parent.tracer();
@@ -174,28 +186,16 @@ final class TraceExchangeFilterFunction implements ExchangeFilterFunction {
 			this.injector = parent.injector();
 			this.tracing = parent.httpTracing().tracing();
 			this.scopePassingTransformer = parent.scopePassingTransformer;
+			this.span = span;
 		}
 
 		@Override
 		public void subscribe(CoreSubscriber<? super ClientResponse> subscriber) {
-			final ClientRequest.Builder builder = ClientRequest.from(this.request);
 
 			Context context = subscriber.currentContext();
 
-			this.next.exchange(builder.build()).subscribe(new WebClientTracerSubscriber(
-					subscriber, context, findOrCreateSpan(builder), this));
-		}
-
-		private Span findOrCreateSpan(ClientRequest.Builder builder) {
-			if (log.isDebugEnabled()) {
-				log.debug("Instrumenting WebClient call");
-			}
-			Span clientSpan = this.handler.handleSend(this.injector, builder,
-					this.request, this.tracer.nextSpan());
-			if (log.isDebugEnabled()) {
-				log.debug("Handled send of " + clientSpan);
-			}
-			return clientSpan;
+			this.next.exchange(request).subscribe(
+					new WebClientTracerSubscriber(subscriber, context, span, this));
 		}
 
 		static final class WebClientTracerSubscriber