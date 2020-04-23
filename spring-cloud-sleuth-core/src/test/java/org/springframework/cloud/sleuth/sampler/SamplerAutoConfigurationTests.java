spring-cloud/spring-cloud-sleuth

commit 16fb8e3ac6cac79d549251b9d87841a3ff180974

Moves responsibility to import SamplerAutoConfiguration to core
Before, spring-cloud-sleuth-zipkin had to import `SamplerAutoConfiguration`
directly to unwind a sampler ordering problem caused by `TraceAutoConfiguration`
defining the default `Sampler` bean.

This fixes that by moving the default `Sampler` to where it belongs
(`SamplerAutoConfiguration`) and having `TraceAutoConfiguration` import
the sampling configuration directly as opposed to relying on auto-configuration
ordering. Finally it removes the mistake of setting `SamplerAutoConfiguration`
as auto-configuration in the first place.

The name `SamplerAutoConfiguration` was left alone because changing it would
interfere with 3rd party code that formerly imported it to correct this issue
in their non-zipkin setups.

Fixes #1618

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/sampler/SamplerAutoConfiguration.java
@@ -20,23 +20,29 @@ import brave.sampler.Sampler;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration
- * Auto-configuration} to setup sampling for Spring Cloud Sleuth.
+ * {@linkplain Configuration configuration} for {@link Sampler}.
  *
  * @author Marcin Grzejszczak
  * @since 2.1.0
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(value = "spring.sleuth.enabled", matchIfMissing = true)
 @EnableConfigurationProperties(SamplerProperties.class)
+// This is not auto-configuration, but it was in the past. Leaving the name as
+// SamplerAutoConfiguration because those not using Zipkin formerly had to
+// import this directly. A less precise name is better than rev-locking code.
 public class SamplerAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	Sampler sleuthTraceSampler() {
+		return Sampler.NEVER_SAMPLE;
+	}
 
 	static Sampler samplerFromProps(SamplerProperties config) {
 		if (config.getProbability() != null) {

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/autoconfig/TraceAutoConfiguration.java
@@ -54,8 +54,10 @@ import org.springframework.cloud.sleuth.DefaultSpanNamer;
 import org.springframework.cloud.sleuth.LocalServiceName;
 import org.springframework.cloud.sleuth.SpanAdjuster;
 import org.springframework.cloud.sleuth.SpanNamer;
+import org.springframework.cloud.sleuth.sampler.SamplerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 
@@ -71,6 +73,7 @@ import org.springframework.util.StringUtils;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(value = "spring.sleuth.enabled", matchIfMissing = true)
 @EnableConfigurationProperties(SleuthProperties.class)
+@Import(SamplerAutoConfiguration.class)
 public class TraceAutoConfiguration {
 
 	/**
@@ -133,12 +136,6 @@ public class TraceAutoConfiguration {
 	@ConditionalOnMissingBean
 	Tracer tracer(Tracing tracing) {
 		return tracing.tracer();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	Sampler sleuthTraceSampler() {
-		return Sampler.NEVER_SAMPLE;
 	}
 
 	@Bean

spring-cloud-sleuth-core/src/main/resources/META-INF/spring.factories
@@ -1,7 +1,6 @@
 # Auto Configuration
 org.springframework.boot.autoconfigure.EnableAutoConfiguration=\
 org.springframework.cloud.sleuth.annotation.SleuthAnnotationAutoConfiguration,\
-org.springframework.cloud.sleuth.sampler.SamplerAutoConfiguration,\
 org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration,\
 org.springframework.cloud.sleuth.log.SleuthLogAutoConfiguration,\
 org.springframework.cloud.sleuth.propagation.SleuthTagPropagationAutoConfiguration,\

spring-cloud-sleuth-zipkin/src/main/java/org/springframework/cloud/sleuth/zipkin2/ZipkinAutoConfiguration.java
@@ -43,7 +43,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
-import org.springframework.cloud.sleuth.sampler.SamplerAutoConfiguration;
 import org.springframework.cloud.sleuth.zipkin2.sender.ZipkinSenderConfigurationImportSelector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -53,8 +52,7 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration
- * Auto-configuration} enables reporting to Zipkin via HTTP. Has a default sampler set
- * from the {@link SamplerAutoConfiguration}
+ * Auto-configuration} enables reporting to Zipkin via HTTP.
  *
  * The {@link ZipkinRestTemplateCustomizer} allows you to customize the
  * {@link RestTemplate} that is used to send Spans to Zipkin. Its default implementation -
@@ -63,7 +61,6 @@ import org.springframework.web.client.RestTemplate;
  * @author Spencer Gibb
  * @author Tim Ysewyn
  * @since 1.0.0
- * @see SamplerAutoConfiguration
  * @see ZipkinRestTemplateCustomizer
  * @see DefaultZipkinRestTemplateCustomizer
  */
@@ -74,7 +71,7 @@ import org.springframework.web.client.RestTemplate;
 @AutoConfigureBefore(TraceAutoConfiguration.class)
 @AutoConfigureAfter(
 		name = "org.springframework.cloud.autoconfigure.RefreshAutoConfiguration")
-@Import({ ZipkinSenderConfigurationImportSelector.class, SamplerAutoConfiguration.class })
+@Import(ZipkinSenderConfigurationImportSelector.class)
 public class ZipkinAutoConfiguration {
 
 	private static final Log log = LogFactory.getLog(ZipkinAutoConfiguration.class);

tests/spring-cloud-sleuth-instrumentation-feign-tests/src/test/java/org/springframework/cloud/sleuth/instrument/feign/issues/issue1125delegates/ManuallyCreatedDelegateLoadBalancerFeignClientTests.java
@@ -130,11 +130,11 @@ class Application {
 	}
 
 	@Bean
-	public AnnotatedFeignClient annotatedFeignClient(Client client, Decoder decoder, Encoder encoder,
-			Contract contract) {
+	public AnnotatedFeignClient annotatedFeignClient(Client client, Decoder decoder,
+			Encoder encoder, Contract contract) {
 		return Feign.builder().client(client).encoder(encoder).decoder(decoder)
-				.contract(contract)
-				.target(new HardCodedTarget<>(AnnotatedFeignClient.class, "foo", "http://foo"));
+				.contract(contract).target(new HardCodedTarget<>(
+						AnnotatedFeignClient.class, "foo", "http://foo"));
 	}
 
 	@Bean
@@ -151,7 +151,8 @@ class Application {
 
 class MyLoadBalancerClient extends LoadBalancerFeignClient {
 
-	MyLoadBalancerClient(Client delegate, CachingSpringLoadBalancerFactory lbClientFactory,
+	MyLoadBalancerClient(Client delegate,
+			CachingSpringLoadBalancerFactory lbClientFactory,
 			SpringClientFactory clientFactory) {
 		super(delegate, lbClientFactory, clientFactory);
 	}

commit 83887a5c5c392e6bc683197bb946294ae66c45e5

Restores special case that ignores sampling properties
Sleuth is unlike most tracing configuration libraries, as it has a
legacy from 1.x as being primarily log correlation. In short, when
Zipkin is not installed, it ignored the sampling properties. This
behavior was managed implicitly through an untested combination of
configuration conventions between the core and zipkin modules.

16fb8e3 broke this and this change puts it back, in a way not tightly
coupled to Zipkin and neither requires the more simple, but confusing
"import SamplerAutoConfiguration" approach. It also backfills tests
that should have broken earlier.

In practice, a site that only uses logging is likely uniform in that, so
whether or not the sampled bit is set is of no consequence. However,
there's a chance that someone might rely on this historical behavior.

We should follow-up on 3.0 and remove this as it is very unintuitive to
intentionally ignore sampling properties. For now, this restores the old
behavior based on heuristics of bean definitions.

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/sampler/SamplerAutoConfiguration.java
@@ -16,6 +16,7 @@
 
 package org.springframework.cloud.sleuth.sampler;
 
+import brave.sampler.CountingSampler;
 import brave.sampler.Sampler;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -23,12 +24,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * {@linkplain Configuration configuration} for {@link Sampler}.
  *
  * @author Marcin Grzejszczak
+ * @see SamplerCondition
  * @since 2.1.0
  */
 @Configuration(proxyBeanMethods = false)
@@ -44,14 +47,17 @@ public class SamplerAutoConfiguration {
 		return Sampler.NEVER_SAMPLE;
 	}
 
+	// NOTE: Brave's default samplers return Sampler.NEVER_SAMPLE if the config implies
+	// that
 	static Sampler samplerFromProps(SamplerProperties config) {
 		if (config.getProbability() != null) {
-			return new ProbabilityBasedSampler(config);
+			return CountingSampler.create(config.getProbability());
 		}
-		return new RateLimitingSampler(config);
+		return brave.sampler.RateLimitingSampler.create(config.getRate());
 	}
 
 	@Configuration(proxyBeanMethods = false)
+	@Conditional(SamplerCondition.class)
 	@ConditionalOnBean(
 			type = "org.springframework.cloud.context.scope.refresh.RefreshScope")
 	protected static class RefreshScopedSamplerConfiguration {
@@ -60,12 +66,18 @@ public class SamplerAutoConfiguration {
 		@RefreshScope
 		@ConditionalOnMissingBean
 		public Sampler defaultTraceSampler(SamplerProperties config) {
-			return samplerFromProps(config);
+			// TODO: Rewrite: refresh should replace the sampler, not change its state
+			// internally
+			if (config.getProbability() != null) {
+				return new ProbabilityBasedSampler(config);
+			}
+			return new RateLimitingSampler(config);
 		}
 
 	}
 
 	@Configuration(proxyBeanMethods = false)
+	@Conditional(SamplerCondition.class)
 	@ConditionalOnMissingBean(
 			type = "org.springframework.cloud.context.scope.refresh.RefreshScope")
 	protected static class NonRefreshScopeSamplerConfiguration {

spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/sampler/SamplerCondition.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.sleuth.sampler;
+
+import brave.TracingCustomizer;
+import brave.handler.FinishedSpanHandler;
+import brave.sampler.Sampler;
+
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+
+/**
+ * Sleuth 1.x optimized for log-correlation only. Unless "spring-cloud-sleuth-zipkin" was
+ * present, the sampler defaulted to {@link Sampler#NEVER_SAMPLE}. This was to ensure the
+ * log only nodes never set the sampled flag.
+ *
+ * <p>
+ * "spring-cloud-sleuth-zipkin" obviated the {@link Sampler#NEVER_SAMPLE} default by
+ * importing {@link SamplerAutoConfiguration}. Nothing else did, so sampling properties
+ * were effectively ignored unless "spring-cloud-sleuth-zipkin" was in use, or something
+ * else similarly imported {@link SamplerAutoConfiguration}.
+ *
+ * <p>
+ * During a review of Wavefront integration, it was considered not correct to have other
+ * code import {@link SamplerAutoConfiguration}. To avoid that, retain the old behaviour
+ * about log only nodes, and also not pin configuration to Zipkin involves a more complex
+ * condition.
+ *
+ * <p>
+ * This condition looks for signs of non-default setup which likely requires sampling
+ * configuration. It passes on one of the following beans exist:
+ *
+ * <p>
+ * <ul>
+ * <li>{@code zipkin2.reporter.Reporter} - what's used by Zipkin or others like
+ * Stackdriver</li>
+ * <li>{@link FinishedSpanHandler} - only accepts sampled data</li>
+ * <li>{@link TracingCustomizer} - can configure one of the above</li>
+ * </ul>
+ *
+ * <p>
+ * An integrated test that shows {@link Sampler#NEVER_SAMPLE} is default on fail exists in
+ * {@code TraceAutoConfigurationTests} intentionally, as users now needn't import
+ * {@link SamplerAutoConfiguration} directly.
+ */
+final class SamplerCondition extends AnyNestedCondition {
+
+	SamplerCondition() {
+		super(ConfigurationPhase.REGISTER_BEAN);
+	}
+
+	// zipkin2.reporter.Reporter is in the classpath here, but technically it is optional
+	@ConditionalOnBean(type = "zipkin2.reporter.Reporter")
+	static final class ReporterAvailable {
+
+	}
+
+	@ConditionalOnBean(FinishedSpanHandler.class)
+	static final class FinishedSpanHandlerAvailable {
+
+	}
+
+	@ConditionalOnBean(TracingCustomizer.class)
+	static final class TracingCustomizerAvailable {
+
+	}
+
+}

spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/autoconfig/TraceAutoConfigurationTests.java
@@ -19,11 +19,14 @@ package org.springframework.cloud.sleuth.autoconfig;
 import brave.propagation.B3Propagation;
 import brave.propagation.ExtraFieldPropagation;
 import brave.propagation.Propagation;
+import brave.sampler.RateLimitingSampler;
+import brave.sampler.Sampler;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import zipkin2.reporter.InMemoryReporterMetrics;
+import zipkin2.reporter.Reporter;
 import zipkin2.reporter.ReporterMetrics;
 import zipkin2.reporter.metrics.micrometer.MicrometerReporterMetrics;
 
@@ -68,8 +71,48 @@ public class TraceAutoConfigurationTests {
 				});
 	}
 
+	/**
+	 * Duplicates
+	 * {@link org.springframework.cloud.sleuth.sampler.SamplerAutoConfigurationTests}
+	 * intentionally, to ensure configuration condition bugs do not exist.
+	 */
 	@Test
-	public void should_use_B3Propagation_factory_if_no_have_any_config() {
+	public void should_use_NEVER_SAMPLER_when_only_logging() {
+		this.contextRunner.run((context -> {
+			final Sampler bean = context.getBean(Sampler.class);
+			BDDAssertions.then(bean).isSameAs(Sampler.NEVER_SAMPLE);
+		}));
+	}
+
+	/**
+	 * Duplicates
+	 * {@link org.springframework.cloud.sleuth.sampler.SamplerAutoConfigurationTests}
+	 * intentionally, to ensure configuration condition bugs do not exist.
+	 */
+	@Test
+	public void should_use_RateLimitedSampler_when_reporting() {
+		this.contextRunner.withUserConfiguration(WithReporter.class).run((context -> {
+			final Sampler bean = context.getBean(Sampler.class);
+			BDDAssertions.then(bean).isInstanceOf(RateLimitingSampler.class);
+		}));
+	}
+
+	/**
+	 * Duplicates
+	 * {@link org.springframework.cloud.sleuth.sampler.SamplerAutoConfigurationTests}
+	 * intentionally, to ensure configuration condition bugs do not exist.
+	 */
+	@Test
+	public void should_override_sampler() {
+		this.contextRunner.withUserConfiguration(WithReporter.class, WithSampler.class)
+				.run((context -> {
+					final Sampler bean = context.getBean(Sampler.class);
+					BDDAssertions.then(bean).isSameAs(Sampler.ALWAYS_SAMPLE);
+				}));
+	}
+
+	@Test
+	public void should_use_B3Propagation_factory_by_default() {
 		this.contextRunner.run((context -> {
 			final Propagation.Factory bean = context.getBean(Propagation.Factory.class);
 			BDDAssertions.then(bean).isInstanceOf(Propagation.Factory.class);
@@ -102,6 +145,26 @@ public class TraceAutoConfigurationTests {
 		@Bean
 		MeterRegistry meterRegistry() {
 			return new SimpleMeterRegistry();
+		}
+
+	}
+
+	@Configuration
+	static class WithReporter {
+
+		@Bean
+		Reporter<zipkin2.Span> spanReporter() {
+			return zipkin2.Span::toString;
+		}
+
+	}
+
+	@Configuration
+	static class WithSampler {
+
+		@Bean
+		Sampler alwaysSampler() {
+			return Sampler.ALWAYS_SAMPLE;
 		}
 
 	}

spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/sampler/SamplerAutoConfigurationTests.java
@@ -16,44 +16,170 @@
 
 package org.springframework.cloud.sleuth.sampler;
 
+import brave.Tracing;
+import brave.TracingCustomizer;
+import brave.handler.FinishedSpanHandler;
+import brave.handler.MutableSpan;
+import brave.propagation.TraceContext;
+import brave.sampler.RateLimitingSampler;
 import brave.sampler.Sampler;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
+import zipkin2.Span;
+import zipkin2.reporter.Reporter;
+
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Marcin Grzejszczak
  * @since
  */
+// TODO: missing spring cloud context tests
 public class SamplerAutoConfigurationTests {
 
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+			.withConfiguration(AutoConfigurations.of(SamplerAutoConfiguration.class));
+
 	@Test
-	public void should_use_probability_sampler_when_property_set() {
-		SamplerProperties properties = new SamplerProperties();
-		properties.setProbability(10f);
-
-		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
-
-		BDDAssertions.then(sampler).isInstanceOf(ProbabilityBasedSampler.class);
+	public void should_use_NEVER_SAMPLER_when_only_logging() {
+		this.contextRunner.run((context -> {
+			final Sampler bean = context.getBean(Sampler.class);
+			BDDAssertions.then(bean).isSameAs(Sampler.NEVER_SAMPLE);
+		}));
 	}
 
 	@Test
-	public void should_use_rate_limiting_sampler_when_probability_not_set() {
+	public void should_use_RateLimitedSampler_withFinishedSpanHandler() {
+		this.contextRunner.withUserConfiguration(WithFinishedSpanHandler.class)
+				.run((context -> {
+					final Sampler bean = context.getBean(Sampler.class);
+					BDDAssertions.then(bean).isInstanceOf(RateLimitingSampler.class);
+				}));
+	}
+
+	@Test
+	public void should_use_RateLimitedSampler_withReporter() {
+		this.contextRunner.withUserConfiguration(WithReporter.class).run((context -> {
+			final Sampler bean = context.getBean(Sampler.class);
+			BDDAssertions.then(bean).isInstanceOf(RateLimitingSampler.class);
+		}));
+	}
+
+	@Test
+	public void should_use_RateLimitedSampler_withTracingCustomizer() {
+		this.contextRunner.withUserConfiguration(WithTracingCustomizer.class)
+				.run((context -> {
+					final Sampler bean = context.getBean(Sampler.class);
+					BDDAssertions.then(bean).isInstanceOf(RateLimitingSampler.class);
+				}));
+	}
+
+	@Test
+	public void should_override_sampler() {
+		this.contextRunner.withUserConfiguration(WithReporter.class, WithSampler.class)
+				.run((context -> {
+					final Sampler bean = context.getBean(Sampler.class);
+					BDDAssertions.then(bean).isSameAs(Sampler.ALWAYS_SAMPLE);
+				}));
+	}
+
+	@Test
+	public void samplerFromProps_probability() {
+		SamplerProperties properties = new SamplerProperties();
+		properties.setProbability(0.01f);
+
+		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
+
+		BDDAssertions.then(sampler).isInstanceOf(brave.sampler.CountingSampler.class);
+	}
+
+	@Test
+	public void samplerFromProps_rateLimit() {
 		SamplerProperties properties = new SamplerProperties();
 
 		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
 
-		BDDAssertions.then(sampler).isInstanceOf(RateLimitingSampler.class);
+		BDDAssertions.then(sampler).isInstanceOf(brave.sampler.RateLimitingSampler.class);
 	}
 
 	@Test
-	public void should_use_probability_sampler_when_both_rate_and_probability_is_set() {
+	public void samplerFromProps_rateLimitZero() {
 		SamplerProperties properties = new SamplerProperties();
-		properties.setProbability(10f);
+		properties.setRate(0);
+
+		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
+
+		BDDAssertions.then(sampler).isSameAs(Sampler.NEVER_SAMPLE);
+	}
+
+	@Test
+	public void samplerFromProps_prefersProbability() {
+		SamplerProperties properties = new SamplerProperties();
+		properties.setProbability(0.01f);
 		properties.setRate(20);
 
 		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
 
-		BDDAssertions.then(sampler).isInstanceOf(ProbabilityBasedSampler.class);
+		BDDAssertions.then(sampler).isInstanceOf(brave.sampler.CountingSampler.class);
+	}
+
+	@Test
+	public void samplerFromProps_prefersZeroProbability() {
+		SamplerProperties properties = new SamplerProperties();
+		properties.setProbability(0.0f);
+		properties.setRate(20);
+
+		Sampler sampler = SamplerAutoConfiguration.samplerFromProps(properties);
+
+		BDDAssertions.then(sampler).isSameAs(Sampler.NEVER_SAMPLE);
+	}
+
+	@Configuration
+	static class WithFinishedSpanHandler {
+
+		@Bean
+		FinishedSpanHandler finishedSpanHandler() {
+			return new FinishedSpanHandler() {
+				@Override
+				public boolean handle(TraceContext context, MutableSpan span) {
+					return true;
+				}
+			};
+		}
+
+	}
+
+	@Configuration
+	static class WithReporter {
+
+		@Bean
+		Reporter<Span> spanReporter() {
+			return zipkin2.Span::toString;
+		}
+
+	}
+
+	@Configuration
+	static class WithSampler {
+
+		@Bean
+		Sampler alwaysSampler() {
+			return Sampler.ALWAYS_SAMPLE;
+		}
+
+	}
+
+	@Configuration
+	static class WithTracingCustomizer {
+
+		@Bean
+		TracingCustomizer tracingCustomizer() {
+			return Tracing.Builder::toString;
+		}
+
 	}
 
 }