674d4752a38cb83cb948bf8658998212551f7cf2
Adrian Cole <adriancole@users.noreply.github.com>
2020-05-16 23:48:38 +0800

    fuzz

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/log/SleuthSlf4jProperties.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/log/SleuthSlf4jProperties.java
@@ -25,7 +25,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Configuration properties for slf4j.
  *
  * @author Arthur Gavlyukovskiy
- * @since 1.0.12 3.0
+ * @since 1.0.12
  */
 @ConfigurationProperties("spring.sleuth.log.slf4j")
 // TODO: Hide in 3.x, if it isn't already deleted
