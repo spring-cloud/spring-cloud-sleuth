73ff81c9ce9fe175add12e7d758bf7dc4c46ea21
Adrian Cole <acole@pivotal.io>
2020-05-26 12:23:47 +0800

    Uses B3 single for non-remote spans (2.2.x)
    This makes sure there cannot be JMS problems related to use of
    hyphenated headers unless someone overrides the `Propagation.Factory`.
    
    To avoid this, we set non-remote spans to B3 single format as done on
    the 3.x branch here:
    https://github.com/spring-cloud/spring-cloud-sleuth/pull/1607/files#diff-db43b7e91bd69d063333c20b947c902bR83-R84
    
    Fixes #1643

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/autoconfig/TraceAutoConfigurationPropagationCustomizationTests.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/autoconfig/TraceAutoConfigurationPropagationCustomizationTests.java
@@ -34,11 +34,6 @@ import org.springframework.context.support.GenericApplicationContext;
 
 public class TraceAutoConfigurationPropagationCustomizationTests {
 
-	// Default for spring-messaging is on 2.2.x is MULTI, though 3.x it is
-	// SINGLE_NO_PARENT spring-cloud/spring-cloud-sleuth#1607
-	Propagation.Factory defaultB3Propagation = B3Propagation.newFactoryBuilder()
-			.injectFormat(Format.MULTI).build();
-
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(TraceAutoConfiguration.class));
 
@@ -46,7 +41,7 @@ public class TraceAutoConfigurationPropagationCustomizationTests {
 	public void stillCreatesDefault() {
 		this.contextRunner.run((context) -> {
 			BDDAssertions.then(context.getBean(Propagation.Factory.class))
-					.isEqualTo(defaultB3Propagation);
+					.isEqualTo(TraceBaggageConfiguration.B3_FACTORY);
 		});
 	}
 
@@ -56,7 +51,7 @@ public class TraceAutoConfigurationPropagationCustomizationTests {
 				.run((context) -> {
 					BDDAssertions.then(context.getBean(Propagation.Factory.class))
 							.hasFieldOrPropertyWithValue("delegate",
-									B3Propagation.FACTORY);
+									TraceBaggageConfiguration.B3_FACTORY);
 				});
 	}
 
@@ -65,7 +60,7 @@ public class TraceAutoConfigurationPropagationCustomizationTests {
 		this.contextRunner.withPropertyValues("spring.application.name=")
 				.run((context) -> {
 					BDDAssertions.then(context.getBean(Propagation.Factory.class))
-							.isEqualTo(B3Propagation.FACTORY);
+							.isEqualTo(TraceBaggageConfiguration.B3_FACTORY);
 				});
 	}
 
