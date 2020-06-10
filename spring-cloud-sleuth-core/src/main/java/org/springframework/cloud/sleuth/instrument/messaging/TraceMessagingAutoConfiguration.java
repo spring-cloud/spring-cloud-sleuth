fdf9f827fa40ac8bd4994c3cfe259f6ee01c0a7e
Roberto Tassi <roberto.tassi@fromlabs.org>
2020-06-10 18:54:54 +0200

    Adds tracing on every factory methods in KafkaListenerContainerFactory

--- spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/messaging/TraceMessagingAutoConfiguration.java
+++ spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/messaging/TraceMessagingAutoConfiguration.java
@@ -302,6 +302,10 @@ class SleuthKafkaAspect {
 	private void anyCreateListenerContainer() {
 	} // NOSONAR
 
+	@Pointcut("execution(public * org.springframework.kafka.config.KafkaListenerContainerFactory.createContainer(..))")
+	private void anyCreateContainer() {
+	} // NOSONAR
+
 	@Around("anyProducerFactory()")
 	public Object wrapProducerFactory(ProceedingJoinPoint pjp) throws Throwable {
 		Producer producer = (Producer) pjp.proceed();
@@ -314,7 +318,7 @@ class SleuthKafkaAspect {
 		return this.kafkaTracing.consumer(consumer);
 	}
 
-	@Around("anyCreateListenerContainer()")
+	@Around("anyCreateListenerContainer() || anyCreateContainer()")
 	public Object wrapListenerContainerCreation(ProceedingJoinPoint pjp)
 			throws Throwable {
 		MessageListenerContainer listener = (MessageListenerContainer) pjp.proceed();
