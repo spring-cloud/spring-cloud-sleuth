f6b40385ab9d5b05bfdb0c4a2a01c4c5168c1c06
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-06-29 18:35:34 +0200

    Create a SleuthMongoConfiguration class to tag Mongodb as a remote service
    fixes gh-1627

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/SleuthTestAutoConfiguration.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/SleuthTestAutoConfiguration.java
@@ -16,7 +16,14 @@
 
 package org.springframework.cloud.sleuth;
 
+import com.mongodb.client.MongoClient;
+import org.mockito.BDDMockito;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -25,6 +32,22 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 class SleuthTestAutoConfiguration {
+
+	@Configuration
+	static class TestMongoConfiguration {
+
+		@Bean
+		@ConditionalOnProperty(value = "test.mongo.mock.enabled", matchIfMissing = true)
+		MongoClient mongoClient() {
+			return BDDMockito.mock(MongoClient.class);
+		}
+
+		@Bean(name = "mongoHealthIndicator")
+		HealthIndicator mongoHealthIndicator() {
+			return () -> Health.up().build();
+		}
+
+	}
 
 	@Configuration
 	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
