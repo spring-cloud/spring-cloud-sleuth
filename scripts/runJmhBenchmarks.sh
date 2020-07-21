bd70edf63585c9f49d52a25eef15dc3fa3986823
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-07-21 19:17:32 +0200

    Improved the way benchmarks are executed

--- scripts/runJmhBenchmarks.sh
+++ scripts/runJmhBenchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running JMH Benchmarks"
-./mvnw clean verify -Djmh.mbr.report.publishTo=csv:http.csv -pl benchmarks -Pbenchmarks
+./mvnw clean install -Pbenchmarks -pl benchmarks --also-make -DskipTests && ./mvnw verify -Djmh.mbr.report.publishTo=csv:http.csv -pl benchmarks -Pbenchmarks
 # java -Djmh.ignoreLock=true -jar benchmarks/target/benchmarks.jar org.springframework.cloud.sleuth.benchmarks.jmh.* -rf csv -rff jmh-result.csv | tee target/benchmarks.log
