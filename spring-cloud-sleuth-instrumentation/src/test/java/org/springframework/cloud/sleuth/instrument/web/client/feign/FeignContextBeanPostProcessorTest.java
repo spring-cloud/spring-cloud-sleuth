61e697d4860c8e3afc83678386a13fb4dc554b45
hash.jang <zhxhash@vip.sina.com>
2021-09-30 01:37:56 +0000

    fix junit test for fixing #2023

--- spring-cloud-sleuth-instrumentation/src/test/java/org/springframework/cloud/sleuth/instrument/web/client/feign/FeignContextBeanPostProcessorTest.java
+++ spring-cloud-sleuth-instrumentation/src/test/java/org/springframework/cloud/sleuth/instrument/web/client/feign/FeignContextBeanPostProcessorTest.java
@@ -1,21 +1,52 @@
+/*
+ * Copyright 2013-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.sleuth.instrument.web.client.feign;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.cloud.openfeign.FeignContext;
 import org.springframework.context.ApplicationContext;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Hash Jang
+ */
+@ExtendWith(MockitoExtension.class)
 class FeignContextBeanPostProcessorTest {
 
+    @Mock
+    BeanFactory beanFactory;
+
+    @Mock
+    ApplicationContext parent;
+
     @Test
-    void should_have_parent_after_post_processed() {
-        FeignContextBeanPostProcessor feignContextBeanPostProcessor = new FeignContextBeanPostProcessor(Mockito.mock(BeanFactory.class));
+    void should_pass_feign_application_context_to_the_trace_representation() {
+        FeignContextBeanPostProcessor feignContextBeanPostProcessor = new FeignContextBeanPostProcessor(beanFactory);
         FeignContext feignContext = new FeignContext();
-        feignContext.setApplicationContext(Mockito.mock(ApplicationContext.class));
+        feignContext.setApplicationContext(parent);
         TraceFeignContext traceFeignContext = (TraceFeignContext) feignContextBeanPostProcessor.postProcessAfterInitialization(feignContext, "test");
-        Assertions.assertNotNull(traceFeignContext.getParent());
+        assertThat(traceFeignContext.getParent()).isNotNull();
+        assertThat(traceFeignContext.getParent()).isSameAs(parent);
     }
+
 }
