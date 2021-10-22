8093f019e14c1ff093560714de1422dcedb3eda6
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-10-22 18:16:22 +0200

    One more checkstyle

--- spring-cloud-sleuth-autoconfigure/src/test/java/org/springframework/cloud/sleuth/autoconfig/instrument/mongodb/TraceMongodbAutoConfigurationTests.java
+++ spring-cloud-sleuth-autoconfigure/src/test/java/org/springframework/cloud/sleuth/autoconfig/instrument/mongodb/TraceMongodbAutoConfigurationTests.java
@@ -24,30 +24,37 @@ import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.cloud.sleuth.autoconfig.TraceNoOpAutoConfiguration;
 import org.springframework.cloud.sleuth.instrument.mongodb.TraceMongoClientSettingsBuilderCustomizer;
 import org.springframework.cloud.sleuth.instrument.mongodb.TraceReactiveMongoClientSettingsBuilderCustomizer;
-import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TraceMongodbAutoConfigurationTests {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-			.withPropertyValues("spring.sleuth.noop.enabled=true").withConfiguration(
-					AutoConfigurations.of(TraceNoOpAutoConfiguration.class, TraceMongoDbAutoConfiguration.class));
+			.withPropertyValues("spring.sleuth.noop.enabled=true")
+			.withConfiguration(AutoConfigurations.of(TraceNoOpAutoConfiguration.class,
+					TraceMongoDbAutoConfiguration.class));
 
 	@Test
 	void should_not_create_reactive_customizer_when_reactive_context_missing() {
 		this.contextRunner
-				.withClassLoader(new FilteredClassLoader(brave.mongodb.MongoDBTracing.class,
+				.withClassLoader(new FilteredClassLoader(
+						brave.mongodb.MongoDBTracing.class,
 						com.mongodb.reactivestreams.client.ReactiveContextProvider.class))
-				.run((context) -> assertThat(context).hasSingleBean(TraceMongoClientSettingsBuilderCustomizer.class)
-						.doesNotHaveBean(TraceReactiveMongoClientSettingsBuilderCustomizer.class));
+				.run((context) -> assertThat(context)
+						.hasSingleBean(TraceMongoClientSettingsBuilderCustomizer.class)
+						.doesNotHaveBean(
+								TraceReactiveMongoClientSettingsBuilderCustomizer.class));
 	}
 
 	@Test
 	void should_create_both_customizers_when_reactive_context_present() {
-		this.contextRunner.withClassLoader(new FilteredClassLoader(brave.mongodb.MongoDBTracing.class))
-				.run((context) -> assertThat(context).hasSingleBean(TraceMongoClientSettingsBuilderCustomizer.class)
-						.hasSingleBean(TraceReactiveMongoClientSettingsBuilderCustomizer.class));
+		this.contextRunner
+				.withClassLoader(
+						new FilteredClassLoader(brave.mongodb.MongoDBTracing.class))
+				.run((context) -> assertThat(context)
+						.hasSingleBean(TraceMongoClientSettingsBuilderCustomizer.class)
+						.hasSingleBean(
+								TraceReactiveMongoClientSettingsBuilderCustomizer.class));
 	}
 
 }
